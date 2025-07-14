ff18d5eea615867153b2708045691a0c7ed68e3b
David Cohen <david.cohen@studio.unibo.it>
2025-07-14 17:54:02 +0200

    test: add unit tests for Entity

---
+++ src/test/scala/io/github/srs/model/EntityTest.scala
@@ -0,0 +1,16 @@
+package io.github.srs.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class EntityTest extends AnyFlatSpec with should.Matchers:
+
+  "Entity trait" should "expose position, shape and orientation" in:
+    val p = Point2D(0.0, 0.0)
+    val o = Orientation(0.0)
+    class Dummy(val position: Point2D,
+                val shape:    ShapeType,
+                val orientation: Orientation) extends Entity
+
+    val e = Dummy(p, ShapeType.Circle(1.0), o)
+    (e.position, e.shape, e.orientation) should be ((p, ShapeType.Circle(1.0), o))
