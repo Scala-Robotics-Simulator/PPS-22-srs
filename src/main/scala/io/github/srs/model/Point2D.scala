7942b0793eb7bf95d47afa3355d4a79e0bce4e30
David Cohen <david.cohen@studio.unibo.it>
2025-07-14 17:40:40 +0200

    test: add unit tests for Point2D class

---
+++ src/main/scala/io/github/srs/model/Point2D.scala
@@ -0,0 +1,10 @@
+package io.github.srs.model
+
+type Point2D = (Double, Double)
+object Point2D:
+  def apply(x: Double, y: Double): Point2D = (x,y)
+  extension (p: Point2D)
+    private def x: Double = p._1
+    private def y: Double = p._2
+    def distanceTo(other: Point2D): Double =
+      math.sqrt(math.pow(other.x - p.x, 2) + math.pow(other.y - p.y, 2))
