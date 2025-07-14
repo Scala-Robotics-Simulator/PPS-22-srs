be725c157f9c5d6cbf57598b22e3b607a47c0e1f
David Cohen <david.cohen@studio.unibo.it>
2025-07-14 17:40:40 +0200

    test: add unit tests for Point2D class

---
+++ src/test/scala/io/github/srs/model/Point2DTest.scala
@@ -0,0 +1,21 @@
+package io.github.srs.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import io.github.srs.model.Point2D.distanceTo
+
+class Point2DTest extends AnyFlatSpec with should.Matchers:
+
+  "Point2D" should "wrap coordinates into a tuple correctly" in:
+    val p = Point2D(1.0, 2.0)
+    p should be ((1.0, 2.0))
+
+  it should "compute the correct Euclidean distance" in:
+    val p1 = Point2D(0.0, 0.0)
+    val p2 = Point2D(3.0, 4.0)
+    p1.distanceTo(p2) should be (5.0)
+
+  it should "be symmetric: distance from p1 to p2 equals distance from p2 to p1" in:
+    val p1 = Point2D(1.0, 1.0)
+    val p2 = Point2D(4.0, 5.0)
+    p1.distanceTo(p2) should be (p2.distanceTo(p1))
