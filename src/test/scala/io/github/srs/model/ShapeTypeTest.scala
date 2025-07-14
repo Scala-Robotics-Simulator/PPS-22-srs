9a99c3e788f336ad6f662707857e17ad7ea26e9c
David Cohen <david.cohen@studio.unibo.it>
2025-07-14 17:56:08 +0200

    refactor: improve formatting and consistency in Entity and related tests

--- src/test/scala/io/github/srs/model/ShapeTypeTest.scala
+++ src/test/scala/io/github/srs/model/ShapeTypeTest.scala
@@ -9,12 +9,12 @@ class ShapeTypeTest extends AnyFlatSpec with should.Matchers:
   "Circle" should "expose its radius" in:
     val c = ShapeType.Circle(2.5)
     c match
-      case ShapeType.Circle(radius) => radius should be (2.5)
-        case _ => fail("Expected Circle shape")
+      case ShapeType.Circle(radius) => radius should be(2.5)
+      case _ => fail("Expected Circle shape")
 
   "Rectangle" should "expose its width and height" in:
     val r = ShapeType.Rectangle(3.0, 4.0)
     r match
       case ShapeType.Rectangle(width, height) =>
-        (width, height) should be (3.0, 4.0)
+        (width, height) should be(3.0, 4.0)
       case _ => fail("Expected Rectangle shape")
