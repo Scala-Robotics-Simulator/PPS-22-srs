e4c7a08fc6f19124e5621f084f885b511f8bfa30
David Cohen <david.cohen@studio.unibo.it>
2025-07-14 17:56:08 +0200

    refactor: improve formatting and consistency in Entity and related tests

--- src/test/scala/io/github/srs/model/EntityTest.scala
+++ src/test/scala/io/github/srs/model/EntityTest.scala
@@ -8,9 +8,7 @@ class EntityTest extends AnyFlatSpec with should.Matchers:
   "Entity trait" should "expose position, shape and orientation" in:
     val p = Point2D(0.0, 0.0)
     val o = Orientation(0.0)
-    class Dummy(val position: Point2D,
-                val shape:    ShapeType,
-                val orientation: Orientation) extends Entity
+    class Dummy(val position: Point2D, val shape: ShapeType, val orientation: Orientation) extends Entity
 
     val e = Dummy(p, ShapeType.Circle(1.0), o)
-    (e.position, e.shape, e.orientation) should be ((p, ShapeType.Circle(1.0), o))
+    (e.position, e.shape, e.orientation) should be((p, ShapeType.Circle(1.0), o))
