2b339e2fda2307ecdeede66693fa2b8cdab90d9b
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-15 11:43:19 +0200

    docs: add documentation for ShapeType and its cases

--- src/main/scala/io/github/srs/model/ShapeType.scala
+++ src/main/scala/io/github/srs/model/ShapeType.scala
@@ -1,5 +1,15 @@
 package io.github.srs.model
 
+/**
+ * ShapeType represents different geometric shapes used in the simulation.
+ */
 enum ShapeType:
+  /**
+   * Circle shape with a specified radius.
+   */
   case Circle(radius: Double)
+
+  /**
+   * Rectangle shape with specified width and height.
+   */
   case Rectangle(width: Double, height: Double)
