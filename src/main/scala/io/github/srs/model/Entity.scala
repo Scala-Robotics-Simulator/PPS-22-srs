4042d272d9352bf6a4ff71efad403652b4ecab50
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-15 12:03:52 +0200

    docs: add documentation for Entity trait and its members

--- src/main/scala/io/github/srs/model/Entity.scala
+++ src/main/scala/io/github/srs/model/Entity.scala
@@ -1,6 +1,32 @@
 package io.github.srs.model
 
+/**
+ * Represents a generic entity in a two-dimensional space.
+ *
+ * An entity is characterized by its position, shape, and orientation.
+ */
 trait Entity:
+  /**
+   * The position of the entity in 2D space.
+   *
+   * @return
+   *   a [[Point2D]] representing the (x, y) coordinates of this entity.
+   */
   def position: Point2D
+
+  /**
+   * The shape type of the entity.
+   *
+   * @return
+   *   the [[ShapeType]] that defines the geometric shape of this entity.
+   */
   def shape: ShapeType
+
+  /**
+   * The orientation of the entity in 2D space.
+   *
+   * @return
+   *   an [[Orientation]] representing the angle or facing direction of this entity.
+   */
   def orientation: Orientation
+end Entity
