6ec3f2cbaffcfccf541170b67447d9125cdc0de7
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-15 11:39:54 +0200

    docs: add Point2D documentation

--- src/main/scala/io/github/srs/model/Point2D.scala
+++ src/main/scala/io/github/srs/model/Point2D.scala
@@ -1,13 +1,29 @@
 package io.github.srs.model
 
+/**
+ * Represents a 2D point.
+ */
 type Point2D = (Double, Double)
 
+/**
+ * Companion object for Point2D, providing an apply method to create instances.
+ */
 object Point2D:
   def apply(x: Double, y: Double): Point2D = (x, y)
 
+/**
+ * Extension methods for Point2D to provide additional functionality.
+ */
 extension (p: Point2D)
   private def x: Double = p._1
   private def y: Double = p._2
 
+  /**
+   * Calculates the Euclidean distance from this point to another point.
+   * @param other
+   *   the other point to which the distance is calculated.
+   * @return
+   *   the Euclidean distance between the two points.
+   */
   def distanceTo(other: Point2D): Double =
     math.sqrt(math.pow(other.x - p.x, 2) + math.pow(other.y - p.y, 2))
