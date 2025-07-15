7fd1eb558a4f4f06cb9742e7d2a40e216fca4501
David Cohen <david.cohen@studio.unibo.it>
2025-07-15 18:45:55 +0200

    docs: enhance documentation for StaticEntity and Validation with detailed descriptions and examples

--- src/main/scala/io/github/srs/model/StaticEntity.scala
+++ src/main/scala/io/github/srs/model/StaticEntity.scala
@@ -3,8 +3,25 @@ package io.github.srs.model
 import io.github.srs.model.validation.Validation
 import io.github.srs.model.validation.Validation.*
 
+/**
+ * Represents a static entity in a two-dimensional space.
+ *
+ * A static entity is characterized by its position, orientation, and shape.
+ */
 enum StaticEntity(val position: Point2D, val orientation: Orientation) extends Entity:
 
+  /**
+   * The [[Obstacle]] represents a rectangular obstacle in the simulation environment.
+   *
+   * @param pos
+   *   center position of the obstacle
+   * @param orient
+   *   orientation of the obstacle
+   * @param width
+   *   width of the obstacle
+   * @param height
+   *   height of the obstacle
+   */
   case Obstacle(
       pos: Point2D,
       orient: Orientation,
@@ -12,6 +29,20 @@ enum StaticEntity(val position: Point2D, val orientation: Orientation) extends E
       height: Double,
   ) extends StaticEntity(pos, orient)
 
+  /**
+   * The [[Light]] represents a light source in the simulation environment.
+   *
+   * @param pos
+   *   center position of the light
+   * @param orient
+   *   orientation of the light
+   * @param radius
+   *   radius of the light's influence
+   * @param intensity
+   *   intensity of the light
+   * @param attenuation
+   *   attenuation factor of the light
+   */
   case Light(
       pos: Point2D,
       orient: Orientation,
@@ -20,14 +51,37 @@ enum StaticEntity(val position: Point2D, val orientation: Orientation) extends E
       attenuation: Double,
   ) extends StaticEntity(pos, orient)
 
+  /**
+   * The shape type of the static entity.
+   *
+   * @return
+   *   the [[ShapeType]] that defines the geometric shape of this static entity.
+   */
   override def shape: ShapeType = this match
     case Obstacle(_, _, w, h) => ShapeType.Rectangle(w, h)
     case Light(_, _, r, _, _) => ShapeType.Circle(r)
 
 end StaticEntity
 
+/**
+ * Companion object for [[StaticEntity]], providing factory methods for creating instances.
+ */
 object StaticEntity:
 
+  /**
+   * Safely build an [[Obstacle]], reflecting the domain constraints.
+   *
+   * @param pos
+   *   center position of the obstacle
+   * @param orient
+   *   orientation of the obstacle
+   * @param width
+   *   width of the obstacle
+   * @param height
+   *   height of the obstacle
+   * @return
+   *   [[Right]] with the created [[StaticEntity.Obstacle]] if valid, otherwise [[Left]] with a validation error.
+   */
   def obstacle(
       pos: Point2D,
       orient: Orientation,
@@ -39,6 +93,21 @@ object StaticEntity:
       h <- positive("height", height)
     yield StaticEntity.Obstacle(pos, orient, w, h)
 
+  /**
+   * Safely build a [[Light]], reflecting the domain constraints.
+   * @param pos
+   *   center position of the light
+   * @param orient
+   *   orientation of the light
+   * @param radius
+   *   radius of the light's influence
+   * @param intensity
+   *   intensity of the light
+   * @param attenuation
+   *   attenuation factor of the light
+   * @return
+   *   [[Right]] with the created [[StaticEntity.Light]] if valid, otherwise [[Left]] with a validation error.
+   */
   def light(
       pos: Point2D,
       orient: Orientation,
