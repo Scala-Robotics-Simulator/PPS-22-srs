d775e30bee5d67b5ab28a431b5b1e8ba36a34f71
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-15 17:19:22 +0200

    feat: extend DynamicEntity to include actuators and update tests

--- src/test/scala/io/github/srs/model/DynamicEntityTest.scala
+++ src/test/scala/io/github/srs/model/DynamicEntityTest.scala
@@ -7,17 +7,23 @@ import org.scalatest.matchers.should.Matchers
 
 class DynamicEntityTest extends AnyFlatSpec with Matchers:
 
+  val p = Point2D(0.0, 0.0)
+  val o = Orientation(0.0)
+  val shape = ShapeType.Circle(1.0)
+
+  class Dummy(
+      val position: Point2D,
+      val shape: ShapeType,
+      val orientation: Orientation,
+      val actuators: Option[Seq[Actuator]],
+  ) extends DynamicEntity
+
   "DynamicEntity" should "support having no actuators" in:
-    val p = Point2D(0.0, 0.0)
-    val o = Orientation(0.0)
-    val shape = ShapeType.Circle(1.0)
-
-    class Dummy(
-        val position: Point2D,
-        val shape: ShapeType,
-        val orientation: Orientation,
-        val actuators: Option[Seq[Actuator]],
-    ) extends DynamicEntity
-
     val entity = new Dummy(p, shape, o, None)
     entity.actuators should be(None)
+
+  it should "support having some actuators" in:
+    val actuator = Actuator.WheelMotor((1.0, 1.0))
+    val entity = new Dummy(p, shape, o, Some(Seq(actuator)))
+    entity.actuators should be(Some(Seq(actuator)))
+end DynamicEntityTest
