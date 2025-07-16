80a67707597fdf69408271c4ef9645e85dff3732
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-16 10:31:32 +0200

    refactor: enhance Wheel implementation to include shape and update tests

--- src/main/scala/io/github/srs/model/entity/dynamic/Wheel.scala
+++ src/main/scala/io/github/srs/model/entity/dynamic/Wheel.scala
@@ -1,11 +1,14 @@
 package io.github.srs.model.entity.dynamic
 
+import io.github.srs.model.entity.ShapeType
+
 trait Wheel:
   def speed: Double
+  def shape: ShapeType.Circle
   def updated(to: Double): Wheel
 
 object Wheel:
-  def apply(speed: Double): Wheel = new WheelImpl(speed)
+  def apply(speed: Double, shape: ShapeType.Circle): Wheel = new WheelImpl(speed, shape)
 
-  private class WheelImpl(val speed: Double) extends Wheel:
-    def updated(to: Double): Wheel = Wheel(to)
+  private class WheelImpl(val speed: Double, val shape: ShapeType.Circle) extends Wheel:
+    override def updated(to: Double): Wheel = Wheel(to, shape)
