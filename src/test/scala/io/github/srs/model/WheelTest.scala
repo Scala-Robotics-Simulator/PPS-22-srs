4197391b6ff13bb84af0bdf2fe9b8149e593115f
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-16 11:14:05 +0200

    test: add unit tests for Wheel implementation

---
+++ src/test/scala/io/github/srs/model/WheelTest.scala
@@ -0,0 +1,22 @@
+package io.github.srs.model
+
+import io.github.srs.model.entity.ShapeType
+import io.github.srs.model.entity.dynamic.Wheel
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class WheelTest extends AnyFlatSpec with Matchers:
+
+  val shape: ShapeType.Circle = ShapeType.Circle(0.05)
+  val wheel: Wheel = Wheel(1.0, shape)
+
+  "Wheel" should "have a speed" in:
+    wheel.speed should be(1.0)
+
+  it should "be updated with a new speed" in:
+    val updatedWheel: Wheel = wheel.updated(2.0)
+    updatedWheel.speed should be(2.0)
+
+  it should "have a circular shape" in:
+    wheel.shape match
+      case ShapeType.Circle(radius) => radius should be(0.05)
