ea559dfe36e715e4b04d8efa3b81098705721fcb
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-17 16:32:16 +0200

    refactor: update Actuator and DynamicEntity traits for type safety

--- src/main/scala/io/github/srs/model/entity/dynamic_entity/Robot.scala
+++ src/main/scala/io/github/srs/model/entity/dynamic_entity/Robot.scala
@@ -4,22 +4,23 @@ import io.github.srs.model.entity.*
 
 trait Robot extends DynamicEntity:
   override def shape: ShapeType.Circle
+  override def actuators: Seq[Actuator[Robot]]
 
 object Robot:
-
-  def apply(
-      position: Point2D,
-      shape: ShapeType.Circle,
-      orientation: Orientation,
-      actuators: Seq[Actuator],
-  ): Robot = RobotImpl(position, shape, orientation, actuators)
-
-  def unapply(robot: Robot): Option[(Point2D, ShapeType.Circle, Orientation, Seq[Actuator])] =
-    Some((robot.position, robot.shape, robot.orientation, robot.actuators))
 
   private case class RobotImpl(
       position: Point2D,
       shape: ShapeType.Circle,
       orientation: Orientation,
-      actuators: Seq[Actuator],
+      actuators: Seq[Actuator[Robot]],
   ) extends Robot
+
+  def apply(
+      position: Point2D,
+      shape: ShapeType.Circle,
+      orientation: Orientation,
+      actuators: Seq[Actuator[Robot]],
+  ): Robot = RobotImpl(position, shape, orientation, actuators)
+
+  def unapply(robot: Robot): Option[(Point2D, ShapeType.Circle, Orientation, Seq[Actuator[Robot]])] =
+    Some((robot.position, robot.shape, robot.orientation, robot.actuators))
--- src/test/scala/io/github/srs/model/entity/dynamic_entity/DynamicEntityTest.scala
+++ src/test/scala/io/github/srs/model/entity/dynamic_entity/DynamicEntityTest.scala
@@ -1,6 +1,5 @@
 package io.github.srs.model.entity.dynamic_entity
 
-import io.github.srs.model.entity.dynamic_entity.{ Actuator, DynamicEntity, Wheel }
 import io.github.srs.model.entity.{ Orientation, Point2D, ShapeType }
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -15,15 +14,19 @@ class DynamicEntityTest extends AnyFlatSpec with Matchers:
       val position: Point2D,
       val shape: ShapeType,
       val orientation: Orientation,
-      val actuators: Seq[Actuator],
-  ) extends DynamicEntity
+      val actuators: Seq[Actuator[Dummy]],
+  ) extends DynamicEntity:
+    def act(): Dummy = this
+
+  class DummyActuator extends Actuator[Dummy]:
+    override def act(entity: Dummy): Dummy = entity
 
   "DynamicEntity" should "support having no actuators" in:
     val entity = new Dummy(p, shape, o, Seq.empty)
     entity.actuators should be(Seq.empty)
 
   it should "support having some actuators" in:
-    val actuator = WheelMotor(0.1, Wheel(1.0, ShapeType.Circle(0.05)), Wheel(1.0, ShapeType.Circle(0.05)))
+    val actuator = new DummyActuator()
     val entity = new Dummy(p, shape, o, Seq(actuator))
     entity.actuators should be(Seq(actuator))
 end DynamicEntityTest
