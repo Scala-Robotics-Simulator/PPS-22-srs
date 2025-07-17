0a69da67b034ac92d30809762c2f260507740b19
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-17 23:36:28 +0200

    chore: introduce Action enum for robot movement and applyActions method in WheelMotor

--- src/test/scala/io/github/srs/model/entity/dynamicentity/RobotTest.scala
+++ src/test/scala/io/github/srs/model/entity/dynamicentity/RobotTest.scala
@@ -5,6 +5,8 @@ import io.github.srs.model.entity.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import io.github.srs.model.entity.dynamicentity.WheelMotor.move
+import io.github.srs.model.entity.dynamicentity.WheelMotor.applyActions
+import io.github.srs.model.entity.dynamicentity.WheelMotorTestUtils.calculateMovement
 
 class RobotTest extends AnyFlatSpec with Matchers:
 
@@ -42,5 +44,48 @@ class RobotTest extends AnyFlatSpec with Matchers:
     val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq.empty)
     val movedRobot: Robot = robot.move
     movedRobot.orientation should be(Orientation(0.0))
+
+  it should "stay at the same position if it has no actions" in:
+    val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+    val movedRobot: Robot = robot.applyActions(Seq.empty)
+    movedRobot.position should be(Point2D(0.0, 0.0))
+
+  it should "return the same orientation if it has no actions" in:
+    val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+    val movedRobot: Robot = robot.applyActions(Seq.empty)
+    movedRobot.orientation should be(Orientation(0.0))
+
+  it should "update its position based on a single MoveForward action" in:
+    val robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+    val movedRobot = robot.applyActions(Seq(Action.MoveForward))
+    val expectedMovement: (Point2D, Orientation) = calculateMovement(robot)
+    movedRobot.position shouldBe expectedMovement._1
+
+  it should "update its orientation based on a single MoveForward action" in:
+    val robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+    val movedRobot = robot.applyActions(Seq(Action.MoveForward))
+    val expectedMovement: (Point2D, Orientation) = calculateMovement(robot)
+    movedRobot.orientation.degrees shouldBe expectedMovement._2.degrees
+
+  it should "update its position based on a sequence of actions" in:
+    val robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+
+    val moved1 = robot.applyActions(Seq(Action.MoveForward))
+    val moved2 = moved1.applyActions(Seq(Action.TurnLeft))
+    val moved3 = moved2.applyActions(Seq(Action.Stop))
+
+    val expectedPosition = moved3.position
+    val movedRobot = robot.applyActions(Seq(Action.MoveForward, Action.TurnLeft, Action.Stop))
+    movedRobot.position shouldBe expectedPosition
+
+  it should "update its orientation based on a sequence of actions" in:
+    val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
+    val moved1 = robot.applyActions(Seq(Action.MoveForward))
+    val moved2 = moved1.applyActions(Seq(Action.TurnLeft))
+    val moved3 = moved2.applyActions(Seq(Action.Stop))
+
+    val expectedOrientation = moved3.orientation
+    val movedRobot = robot.applyActions(Seq(Action.MoveForward, Action.TurnLeft, Action.Stop))
+    movedRobot.orientation.degrees shouldBe expectedOrientation.degrees
 
 end RobotTest
--- src/test/scala/io/github/srs/model/entity/dynamicentity/WheelMotorTest.scala
+++ src/test/scala/io/github/srs/model/entity/dynamicentity/WheelMotorTest.scala
@@ -2,7 +2,7 @@ package io.github.srs.model.entity.dynamicentity
 
 import io.github.srs.model.entity.dynamicentity.WheelMotor.move
 import io.github.srs.model.entity.{ Orientation, Point2D, ShapeType }
-import io.github.srs.model.entity.*
+import io.github.srs.model.entity.dynamicentity.WheelMotorTestUtils.calculateMovement
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -17,44 +17,28 @@ class WheelMotorTest extends AnyFlatSpec with Matchers:
   "WheelMotor" should "update its position based on the wheel speeds" in:
     val leftSpeed: Double = 1.0
     val rightSpeed: Double = 2.0
-
-    val vLeft: Double = leftSpeed * wheelRadius
-    val vRight: Double = rightSpeed * wheelRadius
-    val velocity: Double = (vLeft + vRight) / 2
-    val theta: Double = initialOrientation.toRadians
-    val dx: Double = velocity * math.cos(theta) * dt
-    val dy: Double = velocity * math.sin(theta) * dt
-    val expectedPosition: Point2D = Point2D(initialPosition.x + dx, initialPosition.y + dy)
-
     val wheelMotor: WheelMotor = WheelMotor(
       dt,
       Wheel(leftSpeed, ShapeType.Circle(wheelRadius)),
       Wheel(rightSpeed, ShapeType.Circle(wheelRadius)),
     )
-
     val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
     val movedRobot: Robot = robot.move
 
-    movedRobot.position shouldBe expectedPosition
+    val expectedMovement: (Point2D, Orientation) = calculateMovement(robot)
+    movedRobot.position shouldBe expectedMovement._1
 
   it should "update its orientation based on the wheel speeds" in:
     val leftSpeed: Double = 1.0
     val rightSpeed: Double = 2.0
-
-    val vLeft: Double = leftSpeed * wheelRadius
-    val vRight: Double = rightSpeed * wheelRadius
-    val wheelDistance: Double = shape.radius * 2
-    val omega: Double = (vRight - vLeft) / wheelDistance
-    val expectedOrientation: Orientation = Orientation.fromRadians(initialOrientation.toRadians + omega * dt)
-
     val wheelMotor: WheelMotor = WheelMotor(
       dt,
       Wheel(leftSpeed, ShapeType.Circle(wheelRadius)),
       Wheel(rightSpeed, ShapeType.Circle(wheelRadius)),
     )
-
     val robot: Robot = Robot(initialPosition, shape, initialOrientation, Seq(wheelMotor))
     val movedRobot: Robot = robot.move
 
-    movedRobot.orientation.degrees shouldBe expectedOrientation.degrees
+    val expectedMovement: (Point2D, Orientation) = calculateMovement(robot)
+    movedRobot.orientation.degrees shouldBe expectedMovement._2.degrees
 end WheelMotorTest
