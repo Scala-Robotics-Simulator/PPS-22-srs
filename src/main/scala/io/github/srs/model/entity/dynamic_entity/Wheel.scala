e6614ff4b04f9b5f09949c20f648e11253d2bff6
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-17 12:11:32 +0200

    refactor: update Wheel trait to case class

--- src/main/scala/io/github/srs/model/entity/dynamic_entity/Wheel.scala
+++ src/main/scala/io/github/srs/model/entity/dynamic_entity/Wheel.scala
@@ -2,13 +2,21 @@ package io.github.srs.model.entity.dynamic_entity
 
 import io.github.srs.model.entity.ShapeType
 
-trait Wheel:
-  def speed: Double
-  def shape: ShapeType.Circle
-  def updated(to: Double): Wheel
-
-object Wheel:
-  def apply(speed: Double, shape: ShapeType.Circle): Wheel = new WheelImpl(speed, shape)
-
-  private class WheelImpl(val speed: Double, val shape: ShapeType.Circle) extends Wheel:
-    override def updated(to: Double): Wheel = Wheel(to, shape)
+/**
+ * Represents a wheel with linear speed and circular shape.
+ *
+ * @param speed
+ *   the linear speed of the wheel (in meters per second).
+ * @param shape
+ *   the physical shape of the wheel, assumed to be a circle.
+ */
+final case class Wheel(speed: Double, shape: ShapeType.Circle):
+  /**
+   * Returns a new instance of this wheel with an updated linear speed.
+   *
+   * @param to
+   *   the new linear speed (in meters per second).
+   * @return
+   *   a new [[Wheel]] instance with the updated speed.
+   */
+  def updatedSpeed(to: Double): Wheel = copy(speed = to)
