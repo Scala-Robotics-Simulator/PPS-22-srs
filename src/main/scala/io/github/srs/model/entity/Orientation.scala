f46330958e8e833258275c3b3fceaa262ed26210
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-17 17:52:18 +0200

    refactor: enhance Orientation trait with normalization and additional factory method along with the test

--- src/main/scala/io/github/srs/model/entity/Orientation.scala
+++ src/main/scala/io/github/srs/model/entity/Orientation.scala
@@ -7,7 +7,13 @@ package io.github.srs.model.entity
  */
 trait Orientation:
   /**
-   * The value of this orientation in degrees.
+   * The value of this orientation in degrees, it assu.
+   *
+   * @return
+   *   the orientation angle in degrees.
+   */
+  /**
+   * The value of this orientation in degrees, in the range [0, 360).
    *
    * @return
    *   the orientation angle in degrees.
@@ -22,10 +28,25 @@ trait Orientation:
    */
   def toRadians: Double
 
+end Orientation
+
 /**
  * Companion object for [[Orientation]], providing a factory method.
  */
 object Orientation:
+
+  /**
+   * Normalizes the given angle in degrees to the range [0, 360).
+   *
+   * @param degree
+   *   the angle in degrees to normalize.
+   * @return
+   *   the normalized angle in degrees.
+   */
+  private def normalize(degree: Double): Double =
+    val d = degree % 360
+    if d < 0 then d + 360 else d
+
   /**
    * Creates a new [[Orientation]] instance from an angle in degrees.
    *
@@ -34,7 +55,18 @@ object Orientation:
    * @return
    *   a new [[Orientation]] representing the given angle.
    */
-  def apply(degree: Double): Orientation = OrientationImpl(degree)
+  def apply(degree: Double): Orientation = OrientationImpl(normalize(degree))
+
+  /**
+   * Creates a new [[Orientation]] instance from an angle in radians.
+   *
+   * @param radians
+   *   the angle in radians.
+   * @return
+   *   a new [[Orientation]] representing the given angle.
+   */
+  def fromRadians(radians: Double): Orientation =
+    OrientationImpl(normalize(Math.toDegrees(radians)))
 
   /**
    * Default implementation of [[Orientation]].
@@ -51,5 +83,5 @@ object Orientation:
     /**
      * @inheritdoc
      */
-    override def toRadians: Double = math.toRadians(degree)
+    override def toRadians: Double = Math.toRadians(degree)
 end Orientation
