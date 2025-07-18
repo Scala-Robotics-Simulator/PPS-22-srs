81c8cd6a6d8f275197f67aba05c0c64e8700042d
David Cohen <david.cohen@studio.unibo.it>
2025-07-18 12:24:18 +0200

    refactor: add Cell case class and corresponding test cases

---
+++ src/main/scala/io/github/srs/model/Cell.scala
@@ -0,0 +1,14 @@
+package io.github.srs.model
+
+/** A single square in a grid, defined by its x and y coordinates.
+  * 
+  * Cells represent positions in a grid-based two-dimensional map.
+  * 
+  * The coordinates are zero-indexed, meaning that the top-left cell is at (0, 0).
+  *
+  * @param x
+  *   the coordinate of the cell (horizontal position).
+  * @param y
+  *   the coordinate of the cell (vertical position).  
+ */
+final case class Cell(x: Int, y: Int)
---
+++ src/test/scala/io/github/srs/model/CellTest.scala
@@ -0,0 +1,15 @@
+package io.github.srs.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class CellTest extends AnyFlatSpec with should.Matchers:
+
+  "Cell" should "expose x/y coordinates" in :
+    val c = Cell(2, 3)
+    (c.x, c.y) should be((2, 3))
+
+
+  it should "be created from Point2D" in :
+    val p = Point2D(2.5, 3.7)
+    p.toCell should be(Cell(3, 4))
