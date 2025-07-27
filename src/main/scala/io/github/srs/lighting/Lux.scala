975a623ec8907d65bee267e8739caf0b54f59a7f
david <david.cohen@studio.unibo.it>
2025-07-27 13:53:19 +0200

    feat: add Lux type for illuminance representation

---
+++ src/main/scala/io/github/srs/lighting/Lux.scala
@@ -0,0 +1,10 @@
+package io.github.srs.lighting
+
+/**
+ * Lux is the SI unit of illuminance, measuring luminous flux per unit area. In this implementation, it's represented as
+ * a Double value where:
+ *   - 0.0 represents complete darkness
+ *   - 1.0 represents maximum illumination
+ *   - Intermediate values represent partial illumination
+ */
+type Lux = Double
