19b66dbed72d924a42c3a51811153eec6e02d3c9
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-27 13:13:20 +0200

    docs: add description of Entity trait and its spatial properties

--- docs/docs/04-detailed-design/04-entity.md
+++ docs/docs/04-detailed-design/04-entity.md
@@ -4,6 +4,15 @@ sidebar_position: 4
 
 # Entity
 
+## 
+Il trait `Entity` descrive un’entità spaziale che possiede una posizione bidimensionale (`position: Position2D`), una
+forma (`shape: ShapeType`), rappresentata dall’enumerazione ShapeType e un orientamento (`orientation: Orientation`)
+espresso in gradi.
+Questo trait costituisce l’interfaccia di base per ogni oggetto posizionato nello spazio, fornendo una struttura che
+permette di modellare facilmente oggetti dinamici o statici in uno spazio bidimensionale.
+
+
+
 ![Entity](../../static/img/04-detailed-design/entity.png)
 
 ![Entities](../../static/img/04-detailed-design/entities.png)
---
+++ docs/src/data/sprint1Data.js
@@ -0,0 +1,93 @@
+export const sprint1Data = [
+  // {
+  //   backlogItem: "Come utente, voglio costruire correttamente le entità (statiche e dinamiche), in modo da utilizzarle all'interno della simulazione",
+  //   id: "6.1",
+  //   task: "Modellazione Entity",
+  //   volontario: "Cohen, Nardicchia",
+  //   stima: 0,
+  //   effettivo: 0,
+  //   days: [0, 0, 0, 0, 0, 0, 0, 0]
+  // },
+  // {
+  //   backlogItem: "Come utente, voglio costruire correttamente le entità (statiche e dinamiche), in modo da utilizzarle all'interno della simulazione",
+  //   id: "6.2",
+  //   task: "Modellazione StaticEntity",
+  //   volontario: "Cohen",
+  //   stima: 0,
+  //   effettivo: 0,
+  //   days: [0, 0, 0, 0, 0, 0, 0, 0]
+  // },
+  // {
+  //   backlogItem: "Come utente, voglio costruire correttamente le entità (statiche e dinamiche), in modo da utilizzarle all'interno della simulazione",
+  //   id: "6.3",
+  //   task: "Modellazione DynamicEntity",
+  //   volontario: "Nardicchia",
+  //   stima: 0,
+  //   effettivo: 0,
+  //   days: [0, 0, 0, 0, 0, 0, 0, 0]
+  // },
+  // {
+  //   backlogItem: "Come utente, voglio costruire correttamente le entità (statiche e dinamiche), in modo da utilizzarle all'interno della simulazione",
+  //   id: "6.4",
+  //   task: "Modellazione della validazione",
+  //   volontario: "Cohen",
+  //   stima: 0,
+  //   effettivo: 0,
+  //   days: [0, 0, 0, 0, 0, 0, 0, 0]
+  // },
+  {
+    backlogItem: "Come un utente, voglio modificare la dimensione dell'ambiente",
+    id: "6.1",
+    task: "Modellazione Environment",
+    volontario: "Ceredi",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  {
+    backlogItem: "Come un utente, voglio gestire i robot, così da modificarne il numero e la posizione",
+    id: "7.1",
+    task: "Modellazione Robot",
+    volontario: "Nardicchia",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  {
+    backlogItem: "Come un utente, voglio gestire i robot, così da modificarne il numero e la posizione",
+    id: "7.1",
+    task: "Modellazione Robot",
+    volontario: "Nardicchia",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  {
+    backlogItem: "Come un utente, voglio gestire i robot, così da modificarne il numero e la posizione",
+    id: "8.2",
+    task: "Modellazione Actuator (DifferentialWheelMotor e Wheel)",
+    volontario: "Nardicchia",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  {
+    backlogItem: "Come un utente, voglio gestire i robot, così da modificarne il numero e la posizione",
+    id: "8.3",
+    task: "Modellazione movimento (Action)",
+    volontario: "Nardicchia",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+  {
+    backlogItem: "Come un utente, voglio gestire i robot, così da modificarne il numero e la posizione",
+    id: "8.4",
+    task: "Validazione parametri",
+    volontario: "Nardicchia",
+    stima: 0,
+    effettivo: 0,
+    days: [0, 0, 0, 0, 0, 0, 0, 0]
+  },
+];
+
