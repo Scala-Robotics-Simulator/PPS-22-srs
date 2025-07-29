1a8fccda746e518dad314c907b1e65ad916a3514
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-07-29 21:18:20 +0200

    refactor: add Monix library into dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,25 +1,27 @@
 import sbt.*
 
 object Dependencies {
+
   /*
    * Versions
    */
   private lazy val scalaTestVersion = "3.2.19"
-  private val squidLibVersion  = "3.0.6"
+  private lazy val squidLibVersion = "3.0.6"
+  private lazy val scalaTestPlusVersion = "3.2.19.1"
+  private lazy val monixVersion = "3.4.1"
+
   /*
    * Libraries
    */
   private val scalaTest = "org.scalactic" %% "scalactic" % scalaTestVersion
-  private val scalactic =
-    "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
-  val scalaTestJUnit5 =
-    "org.scalatestplus" %% "junit-5-10" % "3.2.19.1" % "test"
+  private val scalactic = "org.scalatest" %% "scalatest" % scalaTestVersion % "test"
+  val scalaTestPlusJUnit5 = "org.scalatestplus" %% "junit-5-10" % scalaTestPlusVersion % "test"
   val squidLib = "com.squidpony" % "squidlib-util" % squidLibVersion
   val catsCore = "org.typelevel" %% "cats-core" % "2.13.0"
 
   /*
    * Bundles
    */
-  val scalaTestBundle = Seq(scalaTest, scalactic)
+  val scalaTestBundle: Seq[ModuleID] = Seq(scalaTest, scalactic)
 }
 
