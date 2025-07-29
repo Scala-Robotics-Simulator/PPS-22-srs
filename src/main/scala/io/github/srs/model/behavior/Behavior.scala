ae0805d8064e06e00129f17afa08dee32b9acb5f
David Cohen <david.cohen@studio.unibo.it>
2025-07-29 17:14:16 +0200

    feat(behavior): enhance Behavior with constructors, combinators, and extension methods

--- src/main/scala/io/github/srs/model/behavior/Behavior.scala
+++ src/main/scala/io/github/srs/model/behavior/Behavior.scala
@@ -1,14 +1,143 @@
 package io.github.srs.model.behavior
 
-opaque type Behavior[-I,+A] = I => Seq[A]
+import scala.annotation.targetName
 
+/**
+ * A Behavior is a function that takes an input of type `I` and produces a sequence of actions of type `A`.
+ *
+ * @param I
+ *   the input type, which can be any type that provides context for the behavior
+ * @param A
+ *   the output/action type, which can be any type that represents an action or effect
+ */
+opaque type Behavior[-I, +A] = I => Seq[A]
+
+/**
+ * Constructors and combinators for creating and combining behaviors.
+ */
 object Behavior:
 
+  /**
+   * Lift an existing function into a [[Behavior]].
+   *
+   * @param f
+   *   a function from `I` to [[Seq[A]]]
+   * @tparam I
+   *   the input type
+   * @tparam A
+   *   the output/action type
+   * @return
+   *   a [[Behavior]] wrapping `f`
+   */
   inline def apply[I, A](f: I => Seq[A]): Behavior[I, A] = (in: I) => f(in)
+
+  /**
+   * Create a [[Behavior]] that always returns the same action.
+   *
+   * @param a
+   *   the action to return
+   * @tparam I
+   *   the input type
+   * @tparam A
+   *   the output/action type
+   * @return
+   *   a [[Behavior]] that always returns `a`
+   */
   inline def pure[I, A](a: A): Behavior[I, A] = (_: I) => Seq(a)
+
+  /**
+   * Create a [[Behavior]] that produces no actions.
+   *
+   * @tparam I
+   *   the input type
+   * @tparam A
+   *   the output/action type
+   * @return
+   *   a [[Behavior]] that produces an empty sequence of actions
+   */
   def empty[I, A]: Behavior[I, A] = (_: I) => Seq.empty
+
+  /**
+   * Create a [[Behavior]] that executes actions based on a predicate.
+   *
+   * @param p
+   *   a predicate function that takes an input of type `I` and returns a Boolean
+   * @param actions
+   *   the actions to execute if the predicate is true
+   * @tparam I
+   *   the input type
+   * @tparam A
+   *   the output/action type
+   * @return
+   *   a [[Behavior]] that executes `actions` if `p(in)` is true, otherwise produces no actions
+   */
   def when[I, A](p: I => Boolean)(actions: => Seq[A]): Behavior[I, A] =
     (in: I) => if p(in) then actions else Seq.empty
 
+  /**
+   * Extension methods for the `[[Behavior]]` for manipulation and execution.
+   */
   extension [I, A](self: Behavior[I, A])
+    /**
+     * Execute the behavior with the given input.
+     *
+     * @param in
+     *   the input to the behavior
+     * @return
+     *   a sequence of actions produced by the behavior
+     */
     inline def execute(in: I): Seq[A] = self(in)
+    def map[B](f: A => B): Behavior[I, B] = (i: I) => self(i).map(f)
+
+    /**
+     * Filter the actions produced by the behavior based on a predicate.
+     * @param p the predicate function to filter actions
+     * @return 
+     *  a new Behavior that only produces actions satisfying the predicate
+     */
+    def filter(p: A => Boolean): Behavior[I, A] = (i: I) => self(i).filter(p)
+  
+    /**
+     * Combine two behaviors into one.
+     *
+     * @param that
+     *   the other behavior to combine with
+     * @return
+     *   a new [[Behavior]] that produces actions from both behaviors
+     */
+    @targetName("concat")
+    infix def ++(that: Behavior[I, A]): Behavior[I, A] =
+      (i: I) => self(i) ++ that(i)
+
+    /**
+     * Alias for `++`, providing a more readable syntax for combining behaviors.
+     * @param that 
+     *      the other behavior to combine with
+     * @return 
+     *  a new [[Behavior]] that produces actions from both behaviors
+     */
+    infix def andAlso[B](that: Behavior[I, A]): Behavior[I, A] = (i: I) => self(i) ++ that(i)
+  
+    /**
+     * Combine two behaviors such that the second behavior is executed only if the first produces no actions.
+     *
+     * @param that
+     *   the other behavior to combine with
+     * @return
+     *   a new [[Behavior]] that produces actions from the first behavior, or from the second if the first is empty
+     */
+    @targetName("alt")
+    infix def <|>(that: Behavior[I, A]): Behavior[I, A] =
+      (i: I) =>
+        val as = self(i)
+        if as.nonEmpty then as else that(i)
+
+    /**
+     * Alias for `<|>`, providing a more readable syntax for combining behaviors.
+     * @param that 
+     *     the other behavior to combine with
+     * @return
+     *  a new [[Behavior]] that produces actions from the first behavior, or from the second if the first is empty
+     */
+    infix def orElse(that: Behavior[I, A]): Behavior[I, A] = self <|> that
+end Behavior
--- src/test/scala/io/github/srs/behavior/BehaviorTest.scala
+++ src/test/scala/io/github/srs/behavior/BehaviorTest.scala
@@ -5,13 +5,15 @@ import io.github.srs.model.entity.Orientation
 import io.github.srs.model.entity.dynamicentity.Action
 import org.scalatest.OptionValues.*
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.shouldBe
 
+/**
+ * Test suite for the `Behavior` trait and its related functionality.
+ */
 final class BehaviorTest extends AnyFlatSpec:
 
   import io.github.srs.model.behavior.Behavior.*
-  import io.github.srs.model.entity.dynamicentity.Action.{MoveForward, Stop, TurnRight}
-  import io.github.srs.model.entity.dynamicentity.sensor.*
-  import org.scalatest.matchers.should.Matchers.shouldBe
+  import io.github.srs.model.entity.dynamicentity.sensor.{ProximitySensor, SensorReading, SensorReadings}
 
   /** Build a dummy proximity sensor. */
   private def mkSensor() =
@@ -26,19 +28,47 @@ final class BehaviorTest extends AnyFlatSpec:
   given CanEqual[Action, Action] = CanEqual.derived
 
   "Behavior.apply" should "lift a function into a Behavior" in:
-    val b: Behavior[SensorReadings, Action] = Behavior(_ => Seq(Stop))
-    b.execute(readings(0.9)) shouldBe Seq(Stop)
+    val b: Behavior[SensorReadings, Action] = Behavior(_ => Seq(Action.Stop))
+    b.execute(readings(0.9)) shouldBe Seq(Action.Stop)
 
-  "pure" should "always return the given action" in :
-    pure[SensorReadings, Action](MoveForward).execute(readings(0.1, 0.2)) shouldBe Seq(MoveForward)
+  "pure" should "always return the given action" in:
+    pure[SensorReadings, Action](Action.MoveForward).execute(readings(0.1, 0.2)) shouldBe Seq(Action.MoveForward)
 
   "empty" should "produce no actions" in:
     empty[SensorReadings, Action].execute(readings(0.1, 0.2)) shouldBe Seq.empty[Action]
 
   "when(true)" should "emit the provided actions" in:
-      when[SensorReadings, Action](_ => true)(Seq(TurnRight))
-      .execute(readings(0.2)) shouldBe Seq(TurnRight)
+    when[SensorReadings, Action](_ => true)(Seq(Action.TurnRight))
+      .execute(readings(0.2)) shouldBe Seq(Action.TurnRight)
 
   "when(false)" should "produce no actions" in:
-      when[SensorReadings, Action](_ => false)(Seq(Action.TurnRight))
+    when[SensorReadings, Action](_ => false)(Seq(Action.TurnRight))
       .execute(readings(0.2)) shouldBe Seq.empty[Action]
+
+  "map" should "transform produced actions" in :
+    pure[SensorReadings, Action](Action.MoveBackward).map {
+      case Action.MoveBackward => Action.Stop
+      case a => a
+    }.execute(readings()) shouldBe Seq(Action.Stop)
+
+  "filter" should "keep only actions that satisfy predicate" in :
+    Behavior[SensorReadings, Action]((_: SensorReadings) => Seq(Action.MoveForward, Action.Stop))
+      .filter(_ == Action.Stop)
+      .execute(readings()) shouldBe Seq(Action.Stop)
+
+  "++" should "concat two behaviours" in :
+    (pure[SensorReadings, Action](Action.Stop) ++ pure[SensorReadings, Action](Action.MoveForward))
+      .execute(readings()) shouldBe Seq(Action.Stop, Action.MoveForward)
+
+  "andAlso" should "alias ++ (concatenation)" in :
+    (pure[SensorReadings, Action](Action.Stop) andAlso pure[SensorReadings, Action](Action.MoveForward))
+      .execute(readings()) shouldBe Seq(Action.Stop, Action.MoveForward)
+
+  "<|>" should "pick the first non-empty" in :
+    (pure[SensorReadings, Action](Action.MoveForward) <|> pure[SensorReadings, Action](Action.Stop))
+      .execute(readings()) shouldBe Seq(Action.MoveForward)
+
+  "orElse" should "alias <|> (first non-empty wins)" in :
+    (pure[SensorReadings, Action](Action.TurnLeft) orElse pure[SensorReadings, Action](Action.Stop))
+      .execute(readings()) shouldBe Seq(Action.TurnLeft)
+end BehaviorTest
