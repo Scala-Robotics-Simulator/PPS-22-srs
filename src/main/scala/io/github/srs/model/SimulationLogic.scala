cfaee76ab2e305c257ac30fae2886ee3244b0bc7
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-02 17:47:45 +0200

    refactor: implement change time and tick event handling in simulation controller

--- src/main/scala/io/github/srs/model/SimulationLogic.scala
+++ src/main/scala/io/github/srs/model/SimulationLogic.scala
@@ -1,5 +1,7 @@
 package io.github.srs.model
 
+import scala.concurrent.duration.FiniteDuration
+
 import monix.eval.Task
 
 object SimulationLogic:
@@ -8,3 +10,8 @@ object SimulationLogic:
 
     def increment(s: SimulationState): Task[SimulationState] =
       Task(s.copy(i = s.i + 1))
+
+  given ChangeTimeLogic[SimulationState] with
+
+    def changeTime(s: SimulationState, simulationTime: FiniteDuration): Task[SimulationState] =
+      Task(s.copy(i = 0, simulationTime = simulationTime))
---
+++ src/main/scala/io/github/srs/model/TimeLogic.scala
@@ -0,0 +1,14 @@
+package io.github.srs.model
+
+import scala.concurrent.duration.FiniteDuration
+
+import monix.eval.Task
+
+object TimeLogic:
+
+  given TickLogic[SimulationState] with
+
+    def tick(s: SimulationState, delta: FiniteDuration): Task[SimulationState] =
+      val remainingMillis = s.simulationTime.toMillis - delta.toMillis
+      val clamped = if remainingMillis < 0 then 0 else remainingMillis
+      Task.pure(s.copy(simulationTime = FiniteDuration(clamped, scala.concurrent.duration.MILLISECONDS)))
