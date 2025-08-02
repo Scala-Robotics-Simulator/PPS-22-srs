75ff6afdc3d8e44718c5edc744515dd59feaee26
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-02 12:11:57 +0200

    refactor: clean up imports in ProximitySensorTest for better readability

--- src/main/scala/io/github/srs/model/SimulationState.scala
+++ src/main/scala/io/github/srs/model/SimulationState.scala
@@ -1,12 +1,3 @@
 package io.github.srs.model
 
-import monix.eval.Task
-
 final case class SimulationState(i: Int) extends ModelModule.State
-
-object SimulationState:
-
-  given IncrementLogic[SimulationState] with
-
-    def increment(s: SimulationState): Task[SimulationState] =
-      Task(s.copy(i = s.i + 1))
