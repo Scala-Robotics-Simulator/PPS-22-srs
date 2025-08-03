98ecc890e497afa7a7dd7af447a88591a94007ac
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-03 20:14:55 +0200

    refactor: add pause and resume functionality to simulation controller and update event handling

---
+++ src/main/scala/io/github/srs/model/logic/StatusLogic.scala
@@ -0,0 +1,24 @@
+package io.github.srs.model.logic
+
+import io.github.srs.model.{ ModelModule, SimulationState, SimulationStatus }
+import monix.eval.Task
+
+trait PauseLogic[S <: ModelModule.State]:
+  def pause(s: S): Task[S]
+
+trait ResumeLogic[S <: ModelModule.State]:
+  def resume(s: S): Task[S]
+
+trait StopLogic[S <: ModelModule.State]:
+  def stop(s: S): Task[S]
+
+object StatusLogic:
+
+  given pauseLogic: PauseLogic[SimulationState] = (s: SimulationState) =>
+    Task.pure(s.copy(simulationStatus = SimulationStatus.PAUSED))
+
+  given resumeLogic: ResumeLogic[SimulationState] = (s: SimulationState) =>
+    Task.pure(s.copy(simulationStatus = SimulationStatus.RUNNING))
+
+  given stopLogic: StopLogic[SimulationState] = (s: SimulationState) =>
+    Task.pure(s.copy(simulationStatus = SimulationStatus.STOPPED))
--- src/main/scala/io/github/srs/view/SimpleView.scala
+++ src/main/scala/io/github/srs/view/SimpleView.scala
@@ -13,6 +13,8 @@ class SimpleView[S <: ModelModule.State]:
   private val frame = new JFrame("Scala Robotics Simulator")
   private val lblText = new JLabel("Hello World!", SwingConstants.CENTER)
   private val btnIncrement = new JButton("Increment")
+  private val btnPause = new JButton("Pause")
+  private val btnResume = new JButton("Resume")
   private val btnStop = new JButton("Stop")
 
   private def setLabelText(text: String): Unit =
@@ -25,6 +27,8 @@ class SimpleView[S <: ModelModule.State]:
 
     val buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER))
     buttonPanel.add(btnIncrement)
+    buttonPanel.add(btnPause)
+    buttonPanel.add(btnResume)
     buttonPanel.add(btnStop)
     frame.getContentPane.add(buttonPanel, BorderLayout.SOUTH)
 
@@ -34,6 +38,14 @@ class SimpleView[S <: ModelModule.State]:
 
     btnIncrement.addActionListener { _ =>
       queue.offer(Event.Increment).runAsyncAndForget
+    }
+
+    btnPause.addActionListener { _ =>
+      queue.offer(Event.Pause).runAsyncAndForget
+    }
+
+    btnResume.addActionListener { _ =>
+      queue.offer(Event.Resume).runAsyncAndForget
     }
 
     btnStop.addActionListener { _ =>
