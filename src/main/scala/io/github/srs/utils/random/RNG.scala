91e52f187f83163874ee143a1e56e426d14e09bc
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-07 17:35:12 +0200

    feat: implement Rand type and RandomDSL for random value generation

--- src/main/scala/io/github/srs/utils/random/RNG.scala
+++ src/main/scala/io/github/srs/utils/random/RNG.scala
@@ -1,40 +1,168 @@
 package io.github.srs.utils.random
 
+/**
+ * A trait representing a Random Number Generator (RNG).
+ */
 trait RNG:
+
+  /**
+   * Generates a random boolean value along with the next RNG state.
+   * @return
+   *   a tuple containing a random boolean and the next RNG state.
+   */
+  def nextBoolean: (Boolean, RNG)
+
+  /**
+   * Generates a random integer value along with the next RNG state.
+   * @return
+   *   a tuple containing a random integer and the next RNG state.
+   */
   def nextInt: (Int, RNG)
-  def nextIntBetween(min: Int, max: Int): (Int, RNG)
+
+  /**
+   * Generates a random integer value within a specified range (inclusive of min, exclusive of max).
+   * @param min
+   *   the minimum value (inclusive).
+   * @param max
+   *   the maximum value (exclusive).
+   * @return
+   *   a tuple containing a random integer and the next RNG state.
+   */
+  def nextIntBetween(min: Int, max: Int, isMaxExcluded: Boolean = true): (Int, RNG)
+
+  /**
+   * Generates a random double value within the range [0.0, 1.0) along with the next RNG state.
+   * @return
+   *   a tuple containing a random double and the next RNG state.
+   */
   def nextDouble: (Double, RNG)
+
+  /**
+   * Generates a random double value within a specified range.
+   * @param min
+   *   the minimum value (inclusive).
+   * @param max
+   *   the maximum value (exclusive or inclusive based on isMaxExcluded).
+   * @param isMaxExcluded
+   *   whether the maximum value is excluded.
+   * @return
+   *   a tuple containing a random double and the next RNG state.
+   */
   def nextDoubleBetween(min: Double, max: Double, isMaxExcluded: Boolean = true): (Double, RNG)
+
+  /**
+   * Generates a random UUID along with the next RNG state.
+   * @return
+   *   a tuple containing a random UUID and the next RNG state.
+   */
+  def nextUUID: (java.util.UUID, RNG)
+
+  /**
+   * Generates a random long value along with the next RNG state.
+   * @return
+   *   a tuple containing a random long and the next RNG state.
+   */
   def nextLong: (Long, RNG)
+
+  /**
+   * Generates a random string of a specified length.
+   * @param length
+   *   the length of the random string to generate.
+   * @return
+   *   a tuple containing a random string and the next RNG state.
+   */
   def nextString(length: Int): (String, RNG)
 
+end RNG
+
+/**
+ * A simple implementation of the RNG trait using Scala's built-in Random class. This RNG generates random values based
+ * on a seed.
+ * @param seed
+ *   the initial seed for the random number generator.
+ * @note
+ *   This RNG is deterministic, meaning it will produce the same sequence of random values for the same seed.
+ */
 final case class SimpleRNG(seed: Long) extends RNG:
+
+  /**
+   * A random number generator that uses Scala's Random class.
+   */
   private val random = new scala.util.Random(seed)
+
+  /**
+   * Generates a new seed for the RNG.
+   * @return
+   *   a new random seed.
+   */
   private def nextSeed: Long = random.nextLong()
+
+  /**
+   * Creates a new RNG instance with the next seed.
+   * @return
+   *   a new RNG instance with the updated seed.
+   */
   private def nextRNG: RNG = SimpleRNG(nextSeed)
 
+  /**
+   * @inheritdoc
+   */
+  override def nextBoolean: (Boolean, RNG) =
+    val nextBoolean = random.nextBoolean()
+    (nextBoolean, nextRNG)
+
+  /**
+   * @inheritdoc
+   */
   override def nextInt: (Int, RNG) =
     val nextInt = random.nextInt()
     (nextInt, nextRNG)
 
-  override def nextIntBetween(min: Int, max: Int): (Int, RNG) =
-    val nextInt = random.between(min, max)
+  /**
+   * @inheritdoc
+   */
+  override def nextIntBetween(min: Int, max: Int, isMaxExcluded: Boolean = true): (Int, RNG) =
+    val upperBound =
+      if isMaxExcluded then max
+      else if max == Int.MaxValue then max
+      else max + 1
+    val nextInt = random.between(min, upperBound)
     (nextInt, nextRNG)
 
+  /**
+   * @inheritdoc
+   */
   override def nextDouble: (Double, RNG) =
     val nextDouble = random.nextDouble()
     (nextDouble, nextRNG)
 
+  /**
+   * @inheritdoc
+   */
   override def nextDoubleBetween(min: Double, max: Double, isMaxExcluded: Boolean = true): (Double, RNG) =
     val nextDouble = random.nextDouble
-    val scaled = min + (max - min) * nextDouble
-    val result = if !isMaxExcluded && nextDouble > 0.999999999999999 then max else scaled
-    (result, nextRNG)
+    val value =
+      if !isMaxExcluded && nextDouble > 0.999999999999999 then max
+      else min + (max - min) * nextDouble
+    (value, nextRNG)
 
+  /**
+   * @inheritdoc
+   */
+  override def nextUUID: (java.util.UUID, RNG) =
+    val uuid = java.util.UUID.randomUUID()
+    (uuid, nextRNG)
+
+  /**
+   * @inheritdoc
+   */
   override def nextLong: (Long, RNG) =
     val nextLong = random.nextLong()
     (nextLong, nextRNG)
 
+  /**
+   * @inheritdoc
+   */
   override def nextString(length: Int): (String, RNG) =
     val nextString = random.nextString(length)
     (nextString, nextRNG)
---
+++ src/main/scala/io/github/srs/utils/random/RandomDSL.scala
@@ -0,0 +1,110 @@
+package io.github.srs.utils.random
+
+import java.util.UUID
+
+/**
+ * A domain-specific language (DSL) for generating random values using a [[Rand]] type.
+ */
+object RandomDSL:
+
+  /**
+   * Generates a random integer value using the provided [[RNG]].
+   * @return
+   *   a [[Rand]] that generates a random integer.
+   */
+  infix def randomInt: Rand[Int] = (rng: RNG) => rng.nextInt
+
+  /**
+   * Generates a random positive integer value using the provided [[RNG]].
+   * @return
+   *   a [[Rand]] that generates a random positive integer.
+   */
+  infix def positive: Rand[Int] = (rng: RNG) => rng.nextIntBetween(0, Int.MaxValue)
+
+  /**
+   * Generates a random boolean value using the provided [[RNG]].
+   * @return
+   *   a [[Rand]] that generates a random boolean.
+   */
+  infix def randomBoolean: Rand[Boolean] = (rng: RNG) => rng.nextBoolean
+
+  /**
+   * Generates a double value within the range [0.0, 1.0) using the provided [[RNG]].
+   * @return
+   */
+  infix def randomDouble: Rand[Double] = (rng: RNG) => rng.nextDouble
+
+  /**
+   * Generates a long value using the provided [[RNG]].
+   * @return
+   *   a [[Rand]] that generates a random long.
+   */
+  infix def randomLong: Rand[Long] = (rng: RNG) => rng.nextLong
+
+  /**
+   * Generates a random UUID using the provided [[RNG]].
+   * @return
+   *   a [[Rand]] that generates a random UUID.
+   */
+  infix def randomUUID: Rand[UUID] = (rng: RNG) => rng.nextUUID
+
+  extension (rng: RNG)
+    /**
+     * Generates a random value using the provided [[RNG]].
+     * @param rand
+     *   the [[Rand]] to use for generating the random value.
+     * @tparam A
+     *   the type of the random value to generate.
+     * @return
+     *   a tuple containing the generated random value and the next state of the [[RNG]].
+     */
+    infix def generate[A](rand: Rand[A]): (A, RNG) = rand(rng)
+
+  extension (i: Int)
+    /**
+     * Generates a list of random values of type `A` using the provided [[Rand]] and the specified size `i`.
+     * @param ra
+     *   the [[Rand]] to use for generating the random values.
+     * @tparam A
+     *   the type of the random values to generate.
+     * @return
+     *   a [[Rand]] that generates a list of random values of type `A` of size `i`.
+     */
+    infix def times[A](ra: Rand[A]): Rand[List[A]] = Rand.listOfN(i, ra)
+
+    /**
+     * Generates a random string of length `i` using the provided [[RNG]].
+     * @return
+     *   a [[Rand]] that generates a random string of length `i`.
+     */
+    infix def randomString: Rand[String] = rng => rng.nextString(i)
+
+  extension (start: Double)
+
+    /**
+     * Generates a random double value within the range [start, end) or [start, end] based on the `isMaxExcluded`
+     * parameter.
+     * @param end
+     *   the end of the range (exclusive or inclusive based on `isMaxExcluded`).
+     * @return
+     *   a function that takes a boolean indicating whether the maximum value is excluded and returns a [[Rand]] that
+     *   generates a random double.
+     */
+    infix def to(end: Double): Boolean => Rand[Double] =
+      (isMaxExcluded: Boolean) => (rng: RNG) => rng.nextDoubleBetween(start, end, isMaxExcluded)
+
+  extension (f: Boolean => Rand[Double])
+    /**
+     * Generates a random double value within the range [start, end] including the maximum value.
+     * @return
+     *   a [[Rand]] that generates a random double value, including the maximum value.
+     */
+    infix def includeMax: Rand[Double] = f(false)
+
+    /**
+     * Generates a random double value within the range [start, end) excluding the maximum value.
+     * @return
+     *   a [[Rand]] that generates a random double value, excluding the maximum value.
+     */
+    infix def excludeMax: Rand[Double] = f(true)
+end RandomDSL
