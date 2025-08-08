883275f3d7b4f757a1719765be1b2505904ce6d6
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-08 12:11:03 +0200

    refactor: update sprint 2 and backlog items

--- docs/src/data/sprint2Data.js
+++ docs/src/data/sprint2Data.js
@@ -1,4 +1,84 @@
 export const sprint2Data = [
-
+    {
+        backlogItem: "Come utente, voglio gestire gli attuatori di spostamento del robot, come le ruote motorizzate, così da poterli aggiungere, rimuovere o modificare la loro velocità per personalizzare la mobilità del robot",
+        id: "9.3",
+        task: "Modellazione del movimento in azioni",
+        volontario: "Nardicchia",
+        stima: 2,
+        effettivo: 3,
+        days: [0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, durante la simulazione voglio: poter mettere in pausa la simulazione; poter riprendere la simulazione, se in pausa; poter fermare la simulazione; così che possa gestire il controllo della simulazione",
+        id: "16.1",
+        task: "Modellazione del motore di simulazione",
+        volontario: "Nardicchia",
+        stima: 8,
+        effettivo: 5,
+        days: [0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, durante la simulazione voglio: poter mettere in pausa la simulazione; poter riprendere la simulazione, se in pausa; poter fermare la simulazione; così che possa gestire il controllo della simulazione",
+        id: "16.2",
+        task: "Implementazione dello stato della simulazione",
+        volontario: "Nardicchia",
+        stima: 1,
+        effettivo: 3,
+        days: [0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, durante la simulazione voglio: poter mettere in pausa la simulazione; poter riprendere la simulazione, se in pausa; poter fermare la simulazione; così che possa gestire il controllo della simulazione",
+        id: "16.3",
+        task: "Gestione degli eventi di controllo della simulazione",
+        volontario: "Nardicchia",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, durante la simulazione voglio: poter mettere in pausa la simulazione; poter riprendere la simulazione, se in pausa; poter fermare la simulazione; così che possa gestire il controllo della simulazione",
+        id: "16.4",
+        task: "Implementazione della logica di controllo della simulazione",
+        volontario: "Nardicchia",
+        stima: 2,
+        effettivo: 2,
+        days: [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, voglio modificare la durata della simulazione, così da avere simulazioni di durata differente",
+        id: "17.1",
+        task: "Gestione degli eventi temporali della simulazione",
+        volontario: "Nardicchia",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, voglio modificare la durata della simulazione, così da avere simulazioni di durata differente",
+        id: "17.1",
+        task: "Implementazione della logica del tempo (rimanente/trascorso)",
+        volontario: "Nardicchia",
+        stima: 3,
+        effettivo: 2,
+        days: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, voglio poter scegliere il seed, così da poter riprodurre la stessa simulazione",
+        id: "18.1",
+        task: "Implementazione libreria Generatore di Numeri Casuali",
+        volontario: "Nardicchia",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    },
+    {
+        backlogItem: "Deliverable sprint-2 (GUI con visualizzazione dello stato, gestione del tempo e della riproducibilità)",
+        id: "21.1",
+        task: "Implementazione della GUI per la gestione della simulazione",
+        volontario: "Nardicchia",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
 ];
 
