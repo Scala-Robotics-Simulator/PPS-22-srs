2ce09ec548145ee39bfb9ca19afdf8d4b03e290d
David Cohen <david.cohen@studio.unibo.it>
2025-08-08 16:46:32 +0200

    refactor: update lighting package structure

--- src/test/scala/io/github/srs/model/lighting/DiffuserTest.scala
+++ src/test/scala/io/github/srs/model/lighting/DiffuserTest.scala
@@ -1,4 +1,4 @@
-package io.github.srs.lighting
+package io.github.srs.model.lighting
 
 import io.github.srs.model.lighting.Diffuser
 import org.scalatest.flatspec.AnyFlatSpec
--- src/test/scala/io/github/srs/model/lighting/ShadowFovDiffuserTest.scala
+++ src/test/scala/io/github/srs/model/lighting/ShadowFovDiffuserTest.scala
@@ -1,4 +1,4 @@
-package io.github.srs.lighting
+package io.github.srs.model.lighting
 
 import io.github.srs.model.*
 import io.github.srs.model.entity.{ Entity, Orientation }
