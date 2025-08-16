2168818a8708617f7dc52b16e17749c29173a3b4
sceredi <ceredi.simone.iti@gmail.com>
2025-08-16 17:37:31 +0200

    feat: start application by launching simulation

--- src/main/scala/io/github/srs/Launcher.scala
+++ src/main/scala/io/github/srs/Launcher.scala
@@ -1,22 +1,19 @@
 package io.github.srs
 
-import scala.concurrent.duration.{ FiniteDuration, MILLISECONDS }
-import scala.util.Random
-
 import cats.effect.IO
-import cats.effect.unsafe.implicits.global
-import fs2.io.file.Path
-import io.github.srs.config.{ SimulationConfig, YamlConfigManager }
+import io.github.srs.config.SimulationConfig
 import io.github.srs.controller.ControllerModule
 import io.github.srs.controller.ControllerModule.Controller
 import io.github.srs.model.ModelModule.Model
-import io.github.srs.model.SimulationConfig.{ SimulationSpeed, SimulationStatus }
+import io.github.srs.model.SimulationConfig.{SimulationSpeed, SimulationStatus}
+import io.github.srs.model.environment.dsl.CreationDSL.validate
 import io.github.srs.model.logic.simulationStateLogicsBundle
-import io.github.srs.model.{ ModelModule, SimulationState }
+import io.github.srs.model.{ModelModule, SimulationState}
 import io.github.srs.utils.random.SimpleRNG
 import io.github.srs.view.ViewModule
 import io.github.srs.view.ViewModule.View
-import io.github.srs.model.environment.dsl.CreationDSL.validate
+
+import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
 
 /**
  * Launcher object that initializes the simulation.
@@ -30,37 +27,21 @@ object Launcher
   val view: View[SimulationState] = View()
   val controller: Controller[SimulationState] = Controller()
 
-  def run(config: SimulationConfig): IO[Unit] =
-    val seed = config.simulation.seed.getOrElse(Random.nextLong())
-    val duration = config.simulation.duration.map(FiniteDuration(_, MILLISECONDS))
-    val environment = config.environment.validate.getOrElse(
-      sys.exit(1),
-    )
+  def runMVC(state: SimulationState): IO[Unit] =
     for
-      _ <- controller
-        .start(
-          SimulationState(
-            i = 0,
-            simulationTime = duration,
-            simulationSpeed = SimulationSpeed.NORMAL,
-            simulationRNG = SimpleRNG(seed),
-            simulationStatus = SimulationStatus.RUNNING,
-            environment = environment,
-          ),
-        )
+      _ <- controller.start(state)
       _ <- IO.never
     yield ()
-  end run
-end Launcher
 
-object Main:
-
-  @main def run(): Unit =
-    val uri = getClass.getResource("/configuration.yml").toURI
-    val path = Path.fromNioPath(java.nio.file.Paths.get(uri))
-    val loader = YamlConfigManager[IO](path)
-    val result = loader.load.unsafeRunSync()
-    val config = result.getOrElse(
-      sys.exit(1),
-    )
-    Launcher.run(config).unsafeRunSync()
+def mkInitialState(cfg: SimulationConfig): SimulationState =
+  val environment = cfg.environment.validate.getOrElse(
+    sys.exit(1),
+  )
+  SimulationState(
+    i = 0,
+    simulationTime = cfg.simulation.duration.map(FiniteDuration(_, MILLISECONDS)),
+    simulationSpeed = SimulationSpeed.NORMAL,
+    simulationRNG = SimpleRNG(42L),
+    simulationStatus = SimulationStatus.RUNNING,
+    environment = environment,
+  )
