b5703504b5ec6c94fc4013d062052304cd436cc3
David Cohen <david.cohen@studio.unibo.it>
2025-08-17 18:22:27 +0200

    refactor(illumination): simplify LightMap by removing caching and prepared method

--- src/test/scala/io/github/srs/model/illumination/LightMapTest.scala
+++ src/test/scala/io/github/srs/model/illumination/LightMapTest.scala
@@ -8,7 +8,7 @@ import io.github.srs.model.entity.{ Orientation, ShapeType }
 import io.github.srs.model.environment.Environment
 import io.github.srs.model.environment.dsl.CreationDSL.*
 import io.github.srs.model.illumination.engine.SquidLibFovEngine
-import io.github.srs.model.illumination.model.{ LightField, ScaleFactor }
+import io.github.srs.model.illumination.model.{ GridDims, LightField, ScaleFactor }
 import org.scalatest.OptionValues.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -29,56 +29,41 @@ final class LightMapTest extends AnyFlatSpec with Matchers:
    */
   private def env3x3(): Environment =
     val wall = StaticEntity.Obstacle((0.5, 0.5), Orientation(0), width = 1.0, height = 1.0)
-    val bot =
-      (robot at (2.5, 2.5) withShape ShapeType.Circle(math.sqrt(0.5)) withOrientation Orientation(
-        0,
-      )).validate.toOption.value
+    val bot = (robot at (2.5, 2.5) withShape ShapeType.Circle(math.sqrt(0.5)) withOrientation Orientation(
+      0,
+    )).validate.toOption.value
     val light = StaticEntity.Light((1.6, 1.6), Orientation(0), illuminationRadius = 1.3, intensity = 1.0)
-
     (environment withWidth 3 withHeight 3 containing wall containing bot containing light)
       .validate(insertBoundaries = false)
       .toOption
       .value
 
-  /** Run the light map creation and return the prepared instances and light field */
+  /** Run the light map creation and return light field */
   private def runOnce(
       env: Environment,
       scale: ScaleFactor,
-  ): (Illumination.Prepared, Illumination.Prepared, LightField) =
+  ): LightField =
     val io =
       for
         lm <- LightMap.create[IO](SquidLibFovEngine, scale)
-        p1 <- lm.prepared(env)
-        p2 <- lm.prepared(env)
         fld <- lm.computeField(env, includeDynamic = true)
-      yield (p1, p2, fld)
+      yield fld
     io.unsafeRunSync()
 
-  "LightMap" should "memoize Prepared" in:
+  "LightMap" should "produce a field with the expected shape" in:
     val env = env3x3()
-    val (p1, p2, _) = runOnce(env, summon[ScaleFactor])
-    (p1 eq p2) shouldBe true
-
-  it should "produce a field with expected shape" in:
-    val env = env3x3()
-    val (p1, _, fld) = runOnce(env, summon[ScaleFactor])
-    (fld.width, fld.height) shouldBe (p1.dims.widthCells, p1.dims.heightCells)
+    val dims = GridDims.from(env)(summon[ScaleFactor])
+    val fld = runOnce(env, summon[ScaleFactor])
+    (fld.width, fld.height) shouldBe (dims.widthCells, dims.heightCells)
 
   it should "produce intensities within [0,1]" in:
     val env = env3x3()
-    val (_, _, fld) = runOnce(env, summon[ScaleFactor])
+    val fld = runOnce(env, summon[ScaleFactor])
     fld.data.forall(d => d >= 0.0 && d <= 1.0) shouldBe true
 
-  it should "clear the cache" in:
+  it should "be idempotent for the same environment (value equality)" in:
     val env = env3x3()
-    val io =
-      for
-        lm <- LightMap.create[IO](SquidLibFovEngine, summon[ScaleFactor])
-        p1 <- lm.prepared(env)
-        _ <- lm.clear
-        p2 <- lm.prepared(env)
-      yield (p1, p2)
-    val (p1, p2) = io.unsafeRunSync()
-    (p1 eq p2) shouldBe false
-
+    val f1 = runOnce(env, summon[ScaleFactor])
+    val f2 = runOnce(env, summon[ScaleFactor])
+    f1 shouldBe f2
 end LightMapTest
