6be7f4216be9765b712e670b804a6e2252fb75e3
David Cohen <david.cohen@studio.unibo.it>
2025-08-17 19:05:12 +0200

    feat(illumination): add cached LightMap implementation for improved performance

--- src/main/scala/io/github/srs/model/illumination/LightMap.scala
+++ src/main/scala/io/github/srs/model/illumination/LightMap.scala
@@ -1,9 +1,13 @@
 package io.github.srs.model.illumination
 
+import scala.collection.immutable.ListMap
+
 import cats.effect.Sync
+import cats.effect.kernel.Ref
 import io.github.srs.model.environment.Environment
 import io.github.srs.model.illumination.engine.FovEngine
 import io.github.srs.model.illumination.model.{ LightField, ScaleFactor }
+import io.github.srs.model.illumination.utils.StaticSignature
 
 trait LightMap[F[_]]:
   /**
@@ -39,4 +43,40 @@ object LightMap:
         Illumination.field(env, prepared, fov, includeDynamic)
       },
     )
+
+  /**
+   * Create a stateful [[LightMap]] facade that caches static precomputation results
+   *
+   * @param fov
+   *   The Field of View (FoV) engine used for light propagation.
+   * @param scale
+   *   The scale factor for the grid.
+   * @param maxEntries
+   *   Maximum number of cached entries to keep (default is 256).
+   * @tparam F
+   *   The effect type (e.g., IO, Task) used for computations.
+   * @return
+   *   A new [[LightMap]] instance wrapped in the effect type `F`.
+   */
+  def cached[F[_]](fov: FovEngine, scale: ScaleFactor, maxEntries: Int = 256)(using F: Sync[F]): F[LightMap[F]] =
+    F.flatMap(Ref.of[F, ListMap[Int, Illumination.Prepared]](ListMap.empty)) { ref =>
+
+      def getOrBuild(env: Environment): F[Illumination.Prepared] =
+        F.flatMap(F.delay(StaticSignature.of(env, scale))) { sig =>
+          F.flatMap(ref.get) { m =>
+            m.get(sig) match
+              case Some(p) => F.pure(p)
+              case None =>
+                val p = Illumination.prepareStatics(env, scale)
+                val updated = (m + (sig -> p)).takeRight(maxEntries)
+                F.map(ref.set(updated))(_ => p)
+          }
+        }
+
+      F.pure[LightMap[F]] { (env: Environment, includeDynamic: Boolean) =>
+        F.flatMap(getOrBuild(env)) { p =>
+          F.delay(Illumination.field(env, p, fov, includeDynamic))
+        }
+      }
+    }
 end LightMap
