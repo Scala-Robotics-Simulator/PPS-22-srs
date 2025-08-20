ca10da2f9ac9abff4018fc26bcd564c06e52e30f
David Cohen <david.cohen@studio.unibo.it>
2025-08-20 21:42:40 +0200

    refactor(behavior): update Robot to use Behavior type instead of Rule

--- src/main/scala/io/github/srs/model/entity/dynamicentity/Robot.scala
+++ src/main/scala/io/github/srs/model/entity/dynamicentity/Robot.scala
@@ -6,7 +6,7 @@ import cats.effect.IO
 import io.github.srs.model.entity.*
 import io.github.srs.model.entity.dynamicentity.action.{ Action, ActionAlg }
 import io.github.srs.model.entity.dynamicentity.actuator.{ Actuator, DifferentialWheelMotor }
-import io.github.srs.model.entity.dynamicentity.behavior.BehaviorTypes.Rule
+import io.github.srs.model.entity.dynamicentity.behavior.BehaviorTypes.Behavior
 import io.github.srs.model.entity.dynamicentity.dsl.RobotDsl.withActuators
 import io.github.srs.model.entity.dynamicentity.sensor.{ Sensor, SensorReadings }
 import io.github.srs.model.environment.Environment
@@ -28,7 +28,7 @@ final case class Robot(
     override val orientation: Orientation = defaultOrientation,
     override val actuators: Seq[Actuator[Robot]] = defaultActuators,
     override val sensors: Vector[Sensor[Robot, Environment]] = defaultSensors,
-    override val behavior: Rule[IO, SensorReadings, Action[IO]] = defaultBehavior,
+    override val behavior: Behavior[SensorReadings, Action[IO]] = defaultBehavior,
 ) extends DynamicEntity:
 
   // TODO: serialize the id
