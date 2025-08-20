5772a7f89b3e1536cf524abde7db850581cd5733
David Cohen <david.cohen@studio.unibo.it>
2025-08-20 21:39:18 +0200

    refactor(behavior): simplify type aliases

--- src/test/scala/io/github/srs/model/entity/dynamicentity/behavior/BehaviorTypesTest.scala
+++ src/test/scala/io/github/srs/model/entity/dynamicentity/behavior/BehaviorTypesTest.scala
@@ -4,17 +4,16 @@ import cats.data.Kleisli
 import org.scalatest.flatspec.AnyFlatSpec
 
 /**
- * * Unit tests for the BehaviorTypes aliases.
+ * Unit tests for the BehaviorTypes aliases.
  *
  * This test suite verifies the behavior of the Behavior and Condition type aliases.
  */
 final class BehaviorTypesTest extends AnyFlatSpec:
 
-  import cats.Id
   import io.github.srs.model.entity.dynamicentity.behavior.BehaviorTypes.*
 
   "Behavior alias" should "handle numeric input" in:
-    val b: Behavior[Id, Int, String] = Kleisli((i: Int) => (i * 2).toString)
+    val b: Behavior[Int, String] = Kleisli((i: Int) => (i * 2).toString)
     assert(b.run(5) == "10")
 
   "Condition alias" should "return false for negative numbers" in:
