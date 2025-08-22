51f5e6beaf106bb0694f19f51a3ccfd1efc1d8d1
David Cohen <david.cohen@studio.unibo.it>
2025-08-22 14:19:48 +0200

    feat(simulation): introduce centralized state management for simulation view

---
+++ src/main/scala/io/github/srs/view/state/SimulationState.scala
@@ -0,0 +1,36 @@
+package io.github.srs.view.state
+
+import java.awt.image.BufferedImage
+
+import io.github.srs.model.entity.dynamicentity.Robot
+import io.github.srs.model.environment.{ robots, Environment }
+
+/**
+ * Centralized state management for the simulation view eliminating the need for multiple state variables.
+ */
+final case class SimulationState(
+    environment: Option[Environment] = None,
+    selectedRobotId: Option[String] = None,
+    robots: List[Robot] = Nil,
+    staticLayer: Option[BufferedImage] = None,
+    lastCanvasSize: (Int, Int, Int, Int) = (-1, -1, -1, -1),
+):
+
+  def withEnvironment(env: Environment): SimulationState =
+    copy(
+      environment = Some(env),
+      robots = env.robots.sortBy(_.id),
+    )
+
+  def withSelection(id: Option[String]): SimulationState =
+    copy(selectedRobotId = id)
+
+  def withStaticLayer(img: BufferedImage, size: (Int, Int, Int, Int)): SimulationState =
+    copy(staticLayer = Some(img), lastCanvasSize = size)
+
+  def needsStaticLayerUpdate(newSize: (Int, Int, Int, Int)): Boolean =
+    staticLayer.isEmpty || lastCanvasSize != newSize
+
+  def selectedRobot: Option[Robot] =
+    selectedRobotId.flatMap(id => robots.find(_.id.toString == id))
+end SimulationState
