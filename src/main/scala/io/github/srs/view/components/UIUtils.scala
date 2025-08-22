b56f622c1421712d936103df61de8fa992978856
david <david.cohen@studio.unibo.it>
2025-08-22 21:39:23 +0200

    feat(simulation): rename UIStyles to UIUtils

--- src/main/scala/io/github/srs/view/components/UIUtils.scala
+++ src/main/scala/io/github/srs/view/components/UIUtils.scala
@@ -1,15 +1,14 @@
 package io.github.srs.view.components
 
-import java.awt.Toolkit
+import java.awt.{Dimension, Toolkit}
 import javax.swing.BorderFactory
 import javax.swing.border.TitledBorder
-
-import io.github.srs.utils.SimulationDefaults.UI
+import io.github.srs.utils.SimulationDefaults.{Frame, UI}
 
 /**
  * Centralized UI styling to ensure consistency across components
  */
-object UIStyles:
+object UIUtils:
 
   def titledBorder(title: String): javax.swing.border.Border =
     BorderFactory.createCompoundBorder(
@@ -30,7 +29,7 @@ object UIStyles:
   def paddedBorder(padding: Int = UI.Spacing.standardPadding): javax.swing.border.Border =
     BorderFactory.createEmptyBorder(padding, padding, padding, padding)
 
-end UIStyles
+end UIUtils
 
 extension (frame: javax.swing.JFrame)
 
@@ -43,3 +42,10 @@ extension (frame: javax.swing.JFrame)
     val x = (screenSize.width - frameSize.width) / 2
     val y = (screenSize.height - frameSize.height) / 2
     frame.setLocation(x, y)
+
+  /**
+   * Applies default and preferred size to the frame.
+   */
+  def applyDefaultAndPreferSize(): Unit =
+      frame.setMinimumSize(new Dimension(Frame.minWidth, Frame.minHeight))
+      frame.setPreferredSize(new Dimension(Frame.prefWidth, Frame.prefHeight))
