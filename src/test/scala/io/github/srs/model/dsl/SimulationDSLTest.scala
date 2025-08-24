5455d939ad2fa9a3d40e9f5092d811e6131a0f33
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-24 13:41:47 +0200

    chore: enhance SimulationDSL and SimulationGridBuilder to improve robot rendering and movement logic

--- src/test/scala/io/github/srs/model/dsl/SimulationDSLTest.scala
+++ src/test/scala/io/github/srs/model/dsl/SimulationDSLTest.scala
@@ -1,16 +1,26 @@
 package io.github.srs.model.dsl
 
+import scala.concurrent.duration.{ DurationInt, FiniteDuration }
 import scala.language.postfixOps
 
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import cats.implicits.*
 import io.github.srs.model.Simulation.simulation
 import io.github.srs.model.dsl.SimulationDSL.on
+import io.github.srs.model.entity.dynamicentity.Robot
+import io.github.srs.model.entity.dynamicentity.actuator.DifferentialWheelMotor.applyMovementActions
+import io.github.srs.model.entity.dynamicentity.actuator.dsl.DifferentialWheelMotorDsl.*
+import io.github.srs.model.entity.dynamicentity.behavior.Policy
 import io.github.srs.model.entity.dynamicentity.dsl.RobotDsl.*
+import io.github.srs.model.entity.dynamicentity.sensor.Sensor.senseAll
 import io.github.srs.model.entity.staticentity.dsl.ObstacleDsl.*
 import io.github.srs.model.environment.Environment
 import io.github.srs.model.environment.dsl.CreationDSL.*
+import io.github.srs.utils.EqualityGivenInstances.given
+import org.scalactic.Prettifier.default
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalactic.Prettifier.default
 
 class SimulationDSLTest extends AnyFlatSpec with Matchers:
 
@@ -79,6 +89,45 @@ class SimulationDSLTest extends AnyFlatSpec with Matchers:
         |+----+----+----+
         ||    |    |    |
         |+----+----+----+""".stripMargin
+    gridString shouldBe expected
+
+  def stepEnv(env: Environment, dt: FiniteDuration): Environment =
+    env.entities.collect { case r: Robot => r }.toList
+      .foldLeftM(env) { (e, r) =>
+        for
+          sensorReadings <- r.senseAll[IO](e)
+          action = r.behavior.run(sensorReadings)
+          updatedRobot <- r.applyMovementActions[IO](dt, action)
+          newEnv = e.copy(entities = e.entities - r + updatedRobot)
+        yield newEnv
+      }
+      .unsafeRunSync()
+
+  def runSteps(env: Environment, n: Int, dt: FiniteDuration = 10_000.millis): Environment =
+    (0 until n).foldLeft(env) { (currentEnv, _) =>
+      stepEnv(currentEnv, dt)
+    }
+
+  it should "render an environment 3x3 containing a robot moving forward" in:
+    val updatedEnv =
+      env containing (
+        robot at (1, 1)
+          withActuator (differentialWheelMotor ws 1.0)
+          withBehavior Policy.AlwaysForward
+      )
+
+    val finalEnv = runSteps(updatedEnv, 1)
+
+    val gridString = simulation on finalEnv asGrid
+    val expected =
+      """+----+----+----+
+        ||    |    |    |
+        |+----+----+----+
+        ||    |    | R0→|
+        |+----+----+----+
+        ||    |    |    |
+        |+----+----+----+""".stripMargin
+
     gridString shouldBe expected
 
 end SimulationDSLTest
