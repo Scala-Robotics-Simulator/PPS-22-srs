719f6ffc89ad8757219cb699419081395a1fb9e0
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-25 17:47:22 +0200

    docs: add Sprint 4 documentation with goals, tasks, and backlog items

---
+++ docs/src/data/sprint4Data.js
@@ -0,0 +1,39 @@
+export const sprint4Data = [
+    {
+        backlogItem: "Come utente, voglio gestire i fotosensori, in modo aggiungerli, o rimuoverli, dal robot",
+        id: "14.1",
+        task: "Implementazione dell'ambiente di illuminazione",
+        volontario: "Cohen",
+        stima: 0,
+        effettivo: 0,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente, voglio modificare il comportamento dei robot, così da poter personalizzare le loro azioni",
+        id: "15.6",
+        task: "Implementazione dei comportamenti di base",
+        volontario: "Cohen",
+        stima: 0,
+        effettivo: 0,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, voglio poter caricare e salvare un file di configurazione personalizzato della simulazione, così che possa verificare nuovamente comportamenti eseguiti precedentemente",
+        id: "19.5",
+        task: "Implementazione della validazione della configurazione",
+        volontario: "Ceredi",
+        stima: 0,
+        effettivo: 0,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente voglio creare un DSL con cui posso testare facilmente le funzionalità del sistema e i comportamenti dei robot",
+        id: "24.1",
+        task: "Implementazione DSL integrativa per le funzionalità del sistema e i comportamenti dei robot",
+        volontario: "Nardicchia",
+        stima: 0,
+        effettivo: 0,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+];
+
