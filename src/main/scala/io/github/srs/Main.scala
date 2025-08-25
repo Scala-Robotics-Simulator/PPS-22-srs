2558d15b21bf98fffbace892d4fbea32a8bbc4ea
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-25 11:45:36 +0200

    feat: add CLI configuration view and update main launcher logic

--- src/main/scala/io/github/srs/Main.scala
+++ src/main/scala/io/github/srs/Main.scala
@@ -1,14 +1,14 @@
 package io.github.srs
 
-import io.github.srs.view.ConfigurationView
+import io.github.srs.view.{ ConfigurationCLI, ConfigurationGUI, ConfigurationView }
 import cats.effect.unsafe.implicits.global
 
 @main def main(args: String*): Unit =
 
   val headless = args.contains("--headless")
   val launcher = if headless then CLILauncher else GUILauncher
+  val configurationView: ConfigurationView = if headless then ConfigurationCLI() else ConfigurationGUI()
 
-  val configurationView = ConfigurationView()
   val runner = for
     cfg <- configurationView.init()
     state = mkInitialState(cfg)
