17d71a5ebdfc45ec7d9a4b24cb4afd548404f159
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-25 17:10:09 +0200

    docs: update Sprint 3 documentation with goals, deadlines, review, and retrospective insights

--- docs/src/data/sprint3Data.js
+++ docs/src/data/sprint3Data.js
@@ -2,11 +2,11 @@ export const sprint3Data = [
     {
         backlogItem: "Come utente, voglio gestire i fotosensori, in modo aggiungerli, o rimuoverli, dal robot",
         id: "14.1",
-        task: "Implementazione logica di illuminazione dell'ambiente",
+        task: "Creazione API di illuminazione per la gestione dei foto sensori",
         volontario: "Cohen",
-        stima: 6,
-        effettivo: 15,
-        days: [3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+        stima: 1,
+        effettivo: 1,
+        days: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     },
     {
         backlogItem: "Come utente, voglio gestire i fotosensori, in modo aggiungerli, o rimuoverli, dal robot",
@@ -16,6 +16,24 @@ export const sprint3Data = [
         stima: 2,
         effettivo: 1,
         days: [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0] 
+    },
+    {
+        backlogItem: "Come utente, voglio modificare il comportamento dei robot, così da poter personalizzare le loro azioni",
+        id: "15.5",
+        task: "Semplificazione e adattamento della logica dei comportamenti",
+        volontario: "Cohen",
+        stima: 3,
+        effettivo: 2,
+        days: [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come un utente, durante la simulazione voglio: poter mettere in pausa la simulazione; poter riprendere la simulazione, se in pausa; poter fermare la simulazione; così che possa gestire il controllo della simulazione",
+        id: "16.5",
+        task: "Implementazione della logica di evento per il movimento e la collisione dei robot a ogni step di simulazione",
+        volontario: "Nardicchia",
+        stima: 5,
+        effettivo: 5,
+        days: [0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 0, 0, 0, 0]
     },
     {
         backlogItem: "Come un utente, voglio poter caricare e salvare un file di configurazione personalizzato della simulazione, così che possa verificare nuovamente comportamenti eseguiti precedentemente",
@@ -38,6 +56,15 @@ export const sprint3Data = [
     {
         backlogItem: "Come un utente, voglio poter scegliere una configurazione della simulazione, così che possa osservare determinati comportamenti.",
         id: "21.2",
+        task: "Implementazione della CLI di configurazione",
+        volontario: "Nardicchia",
+        stima: 3,
+        effettivo: 2,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0]
+    },
+    {
+        backlogItem: "Come un utente, voglio poter scegliere una configurazione della simulazione, così che possa osservare determinati comportamenti.",
+        id: "21.3",
         task: "Caricamento della configurazione",
         volontario: "Ceredi",
         stima: 2,
@@ -46,12 +73,48 @@ export const sprint3Data = [
     },
     {
         backlogItem: "Come un utente, voglio poter scegliere una configurazione della simulazione, così che possa osservare determinati comportamenti.",
-        id: "21.3",
+        id: "21.4",
         task: "Preparazione configurazioni di default",
         volontario: "Ceredi",
         stima: 2,
         effettivo: 1,
         days: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente voglio visualizzare l'andamento della simulazione in tempo reale, per studiare il comportamento dei robot",
+        id: "22.1",
+        task: "Implementazione del canvas per la visualizzazione della simulazione",
+        volontario: "Cohen",
+        stima: 8,
+        effettivo: 10,
+        days: [0, 0, 0, 0, 1, 2, 0, 0, 0, 1, 2, 2, 2, 0]
+    },
+    {
+        backlogItem: "Come utente voglio visualizzare l'andamento della simulazione in tempo reale, per studiare il comportamento dei robot",
+        id: "22.2",
+        task: "Implementazione CLI simulazione",
+        volontario: "Nardicchia",
+        stima: 3,
+        effettivo: 3,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente voglio visualizzare l'andamento della simulazione in tempo reale, per studiare il comportamento dei robot",
+        id: "22.3",
+        task: "Implementazione del pannello di stato della simulazione e dei dettagli dei singoli robot",
+        volontario: "Cohen",
+        stima: 2,
+        effettivo: 2,
+        days: [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente voglio monitorare i dettagli dei singoli robot in tempo reale, per studiarne il comportamento",
+        id: "23.1",
+        task: "Semplificazione e adattamento della logica dei comportamenti",
+        volontario: "Cohen",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
     },
 ];
 
