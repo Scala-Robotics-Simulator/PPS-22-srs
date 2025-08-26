897a32b100e9ad6ea5c303b12e7362e75c7a105a
David Cohen <david.cohen@studio.unibo.it>
2025-08-26 15:24:09 +0200

    feat(behavior): add BehaviorContext for behavior evaluation input

---
+++ src/main/scala/io/github/srs/model/entity/dynamicentity/behavior/BehaviorContext.scala
@@ -0,0 +1,12 @@
+package io.github.srs.model.entity.dynamicentity.behavior
+
+import io.github.srs.model.entity.dynamicentity.sensor.SensorReadings
+import io.github.srs.utils.random.RNG
+
+/**
+ * Input context for behavior evaluation. Designed to be extended as new behaviors need more information.
+ */
+final case class BehaviorContext(
+    sensorReadings: SensorReadings,
+    rng: RNG,
+)
