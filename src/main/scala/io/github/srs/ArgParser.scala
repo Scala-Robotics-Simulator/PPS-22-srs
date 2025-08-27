495733f397e3a114545d608915e6f1be7dca0497
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-27 10:26:39 +0200

    docs: add documentation comments for AppArgs and ArgParser classes

--- src/main/scala/io/github/srs/ArgParser.scala
+++ src/main/scala/io/github/srs/ArgParser.scala
@@ -2,14 +2,23 @@ package io.github.srs
 
 import scopt.OParser
 
+/**
+ * Command-line argument parser for the Scala Robotics Simulator application.
+ */
 object ArgParser:
 
   private val manifest = getClass.getPackage
   private val appVersion = Option(manifest.getImplementationVersion).getOrElse("dev")
   private val appTitle = Option(manifest.getImplementationTitle).getOrElse("SRS")
 
+  /**
+   * The command-line arguments builder.
+   */
   private val builder = OParser.builder[AppArgs]
 
+  /**
+   * The command-line argument parser.
+   */
   private val parser =
     import builder.*
     OParser.sequence(
@@ -33,9 +42,16 @@ object ArgParser:
       help("help").text("Print this help message"),
       version("version").text("Show version"),
     )
-
   end parser
 
+  /**
+   * Parses the command-line arguments.
+   *
+   * @param args
+   *   the command-line arguments.
+   * @return
+   *   Some(AppArgs) if parsing was successful, None otherwise.
+   */
   def parse(args: Seq[String]): Option[AppArgs] =
     OParser.parse(parser, args, AppArgs())
 end ArgParser
