b2093cd5c715842992409e1efff0a4568fa64709
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-08-30 13:58:30 +0200

    refactor: add environment configuration and simulation execution methods

--- src/main/scala/io/github/srs/config/SimulationConfig.scala
+++ src/main/scala/io/github/srs/config/SimulationConfig.scala
@@ -1,7 +1,11 @@
 package io.github.srs.config
 
-import io.github.srs.model.Simulation
+import io.github.srs.CLILauncher.runMVC
+import io.github.srs.mkInitialState
+import io.github.srs.model.{ Simulation, SimulationState }
 import io.github.srs.model.environment.EnvironmentParameters
+import io.github.srs.model.environment.ValidEnvironment.ValidEnvironment
+import cats.effect.unsafe.implicits.global
 
 /**
  * A configuration for a simulation, containing the simulation and its environment.
@@ -17,3 +21,18 @@ final case class SimulationConfig[E <: EnvironmentParameters](
     simulation: Simulation,
     environment: E,
 )
+
+extension (simulationConfig: SimulationConfig[ValidEnvironment])
+
+  /**
+   * Runs the simulation in headless mode and returns the final state if the duration is defined.
+   * @return
+   *   the final state of the simulation wrapped in Some, or None if the duration is not defined.
+   */
+  infix def run: Option[SimulationState] =
+
+    val initialState = mkInitialState(simulationConfig, headless = true)
+
+    simulationConfig.simulation.duration match
+      case None => None
+      case _ => Some(runMVC(initialState).unsafeRunSync())
