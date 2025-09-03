69375eeb9b152cf1db23756b3bcbe4098f6b984c
david <david.cohen@studio.unibo.it>
2025-09-03 18:24:36 +0200

    docs: update sprint4Data

--- docs/src/data/sprint4Data.js
+++ docs/src/data/sprint4Data.js
@@ -9,22 +9,40 @@ export const sprint4Data = [
         days: [0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     },
     {
-        backlogItem: "Come utente, voglio gestire i fotosensori, in modo aggiungerli, o rimuoverli, dal robot",
-        id: "14.1",
-        task: "Implementazione dell'ambiente di illuminazione",
+        backlogItem: "Come utente, voglio gestire le luci, così da configurare il numero, la posizione e l'intensità di essi",
+        id: "11.5",
+        task: "Refactoring e ottimizzazione del motore di illuminazione (LightMap, occlusione, FOV)",
         volontario: "Cohen",
-        stima: 0,
-        effettivo: 0,
-        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        stima: 8,
+        effettivo: 8,
+        days: [1, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente voglio visualizzare l'andamento della simulazione in tempo reale, per studiare il comportamento dei robot",
+        id: "22.4",
+        task: "Implementazione pannello ‘Tempo di simulazione’ (GUI) con binding dello stato",
+        volontario: "Cohen",
+        stima: 1,
+        effettivo: 1,
+        days: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     },
     {
         backlogItem: "Come utente, voglio modificare il comportamento dei robot, così da poter personalizzare le loro azioni",
         id: "15.6",
-        task: "Implementazione dei comportamenti di base",
+        task: "Refactoring del framework dei Behavior e del DSL di composizione",
         volontario: "Cohen",
-        stima: 0,
-        effettivo: 0,
-        days: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        stima: 2,
+        effettivo: 2,
+        days: [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        backlogItem: "Come utente, voglio modificare il comportamento dei robot, così da poter personalizzare le loro azioni",
+        id: "15.7",
+        task: "Implementazione delle policy comportamentali (ObstacleAvoidance, Phototaxis, RandomWalk, Prioritized)",
+        volontario: "Cohen",
+        stima: 5,
+        effettivo: 5,
+        days: [0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0]
     },
     {
         backlogItem: "Come un utente, voglio poter caricare e salvare un file di configurazione personalizzato della simulazione, così che possa verificare nuovamente comportamenti eseguiti precedentemente",
