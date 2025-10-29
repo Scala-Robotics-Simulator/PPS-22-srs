5f90df53ea1a9519946162c1713efae5b1b7e18f
david <david.cohen@studio.unibo.it>
2025-10-29 12:08:21 +0100

    feat: add support for reinforcement learning server mode with a configurable port

--- src/main/scala/io/github/srs/RLLauncher.scala
+++ src/main/scala/io/github/srs/RLLauncher.scala
@@ -11,7 +11,23 @@ import io.github.srs.controller.protobuf.rl.RLServiceModule
 import io.github.srs.controller.protobuf.rl.RLServerModule
 import io.github.srs.controller.protobuf.rl.RLServerModule.Server
 
-object RLLauncher
+/** Factory object for creating RLLauncher instances */
+object RLLauncher:
+  /**
+   * Creates a new RLLauncher with the specified port
+   * @param port
+   *   the port number for the gRPC server (default: 50051)
+   * @return
+   *   a new RLLauncher instance
+   */
+  def apply(port: Int = 50051): RLLauncher = new RLLauncher(port)
+
+/**
+ * RL Launcher that starts a gRPC server for reinforcement learning
+ * @param port
+ *   the port number for the gRPC server
+ */
+class RLLauncher(port: Int)
     extends ModelModule.Interface[BaseSimulationState]
     with RLControllerModule.Interface[BaseSimulationState]
     with RLServiceModule.Interface[BaseSimulationState]
@@ -19,6 +35,6 @@ object RLLauncher
   val model = Model()
   val controller = Controller()
   val service = Service()
-  val server = Server(50051)
+  val server = Server(port)
 
-  def run: IO[Unit] = IO.println("Starting rl-server") *> server.run
+  def run: IO[Unit] = IO.println(s"Starting rl-server on port $port") *> server.run
