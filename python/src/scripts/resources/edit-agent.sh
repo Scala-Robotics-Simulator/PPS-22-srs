512859cf1015a10fa803419db71fd2aa2752350f
david <david.cohen@studio.unibo.it>
2025-11-04 18:19:13 +0100

    refactor: update YAML configuration for Phototaxis agent rewards and termination criteria

--- python/src/scripts/resources/edit-agent.sh
+++ python/src/scripts/resources/edit-agent.sh
@@ -1,5 +1,6 @@
-find ./obstacle-avoidance -type f -name "*.yml" -exec sed -i \
-  -e 's/reward: .*/reward: NewRewardValue/' \
-  -e 's/termination: .*/termination: NewTerminationValue/' \
-  -e 's/truncation: .*/truncation: NewTruncationValue/' \
+find ./generated/phototaxis/conf -type f -name "*.yml" -exec sed -i \
+  -e 's/reward: .*/reward: Phototaxis/' \
+  -e 's/termination: .*/termination: LightReached/' \
+  -e 's/truncation: .*/truncation: CollisionContact/' \
+  -e 's/illuminationRadius: .*/illuminationRadius: 6.0/' \
   {} +
