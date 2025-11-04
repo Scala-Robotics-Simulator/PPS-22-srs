de579eefc24a2f9299203ff81cc8168b7d0dd390
david <david.cohen@studio.unibo.it>
2025-11-04 22:21:56 +0100

    refactor: update termination and truncation parameters in agent configuration

--- python/src/scripts/resources/edit-agent.sh
+++ python/src/scripts/resources/edit-agent.sh
@@ -1,6 +1,6 @@
 find ./generated/phototaxis/conf -type f -name "*.yml" -exec sed -i \
   -e 's/reward: .*/reward: Phototaxis/' \
-  -e 's/termination: .*/termination: LightReached/' \
-  -e 's/truncation: .*/truncation: CollisionContact/' \
-  -e 's/illuminationRadius: .*/illuminationRadius: 6.0/' \
+  -e 's/termination: .*/termination: CrashOrReached/' \
+  -e 's/truncation: .*/truncation: NeverTruncate/' \
+  -e 's/illuminationRadius: .*/illuminationRadius: 7.0/' \
   {} +
