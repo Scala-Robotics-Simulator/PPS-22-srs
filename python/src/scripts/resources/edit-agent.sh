037f70aaf80de2f51b77c6da8637b8913686e9d4
david <david.cohen@studio.unibo.it>
2025-11-05 14:46:16 +0100

    refactor: adjust illumination radius and update proximity thresholds in training scripts

--- python/src/scripts/resources/edit-agent.sh
+++ python/src/scripts/resources/edit-agent.sh
@@ -2,5 +2,5 @@ find ./generated/phototaxis/conf -type f -name "*.yml" -exec sed -i \
   -e 's/reward: .*/reward: Phototaxis/' \
   -e 's/termination: .*/termination: CrashOrReached/' \
   -e 's/truncation: .*/truncation: NeverTruncate/' \
-  -e 's/illuminationRadius: .*/illuminationRadius: 7.0/' \
+  -e 's/illuminationRadius: .*/illuminationRadius: 6.0/' \
   {} +
