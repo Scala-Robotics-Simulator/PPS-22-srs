80a2ff52c48a886d6c1c26b4e113a4bf0a6363a6
sceredi <ceredi.simone.iti@gmail.com>
2025-11-13 09:23:30 +0100

    feat: add smoothed td loss plotting

--- python/src/evaluation/agent_evaluation.py
+++ python/src/evaluation/agent_evaluation.py
@@ -16,15 +16,18 @@ def evaluate(
     configs: str,
     max_steps: int,
     did_succeed: Callable[[float, bool, bool], bool],
-) -> tuple[dict[str, float], dict[str, float]]:
+):
     successes = dict.fromkeys(agents.keys(), 0)
+    successes_idx = dict.fromkeys(agents.keys(), [])
     steps_to_success = dict.fromkeys(agents.keys(), [])
+    td_losses = dict.fromkeys(agents.keys(), [])
     for config_idx in trange(len(configs), desc="Evaluation", unit="configuration run"):
         env.init(configs[config_idx])
         obs, _ = env.reset()
         done = False
         step = 0
         total_rewards = dict.fromkeys(agents.keys(), 0)
+        episode_td_losses = dict.fromkeys(agents.keys(), [])
         while not done and step < max_steps:
             actions = {
                 k: agents[k].choose_action(v, epsilon_greedy=False)
@@ -32,6 +35,20 @@ def evaluate(
             }
 
             next_obs, rewards, terminateds, truncateds, _ = env.step(actions)
+
+            for agent_id, agent in agents.items():
+                if isinstance(agent, DQAgent):
+                    state = obs[agent_id]
+                    action = actions[agent_id]
+                    reward = rewards[agent_id]
+                    next_state = next_obs[agent_id]
+                    done_flag = terminateds[agent_id] or truncateds[agent_id]
+
+                    td_loss = agent.compute_td_loss(
+                        state, action, reward, next_state, done_flag
+                    )
+                    # td_losses[agent_id].append(td_loss)
+                    episode_td_losses[agent_id].append(td_loss)
 
             dones = {
                 agent_id: terminateds[agent_id] or truncateds[agent_id]
@@ -47,12 +64,20 @@ def evaluate(
                         truncateds[k] or step == max_steps,
                     ):
                         successes[k] += 1
+                        successes_idx[k].append(config_idx)
                         steps_to_success[k].append(step)
             done = all(dones.values())
             obs = next_obs
+        for agent_id in agents.keys():
+            td_losses[agent_id].append(episode_td_losses[agent_id])
     success_rate = {agent_id: v / len(configs) for agent_id, v in successes.items()}
     median_steps_to_success = {
         agent_id: np.median(np.array(v)) for agent_id, v in steps_to_success.items()
     }
 
-    return (success_rate, median_steps_to_success)
+    return {
+        "success_rate": success_rate,
+        "successes_idx": successes_idx,
+        "median_steps_to_success": median_steps_to_success,
+        "td_losses": td_losses,
+    }
