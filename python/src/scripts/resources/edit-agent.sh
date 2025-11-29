8ac712f98726e9426748354e7069608a2c2f9499
david <david.cohen@studio.unibo.it>
2025-11-29 15:51:42 +0100

    refactor: update illumination radius parameters and optimize reward functions for phototaxis
    # Conflicts:
    #	python/src/scripts/generate_environments.py

--- python/src/scripts/resources/edit-agent.sh
+++ python/src/scripts/resources/edit-agent.sh
@@ -1,6 +1,5 @@
-find ./generated/phototaxis/conf -type f -name "*.yml" -exec sed -i \
+find ./generated/phototaxis/envs1 -type f -name "*.yml" -exec sed -i \
   -e 's/reward: .*/reward: Phototaxis/' \
   -e 's/termination: .*/termination: CrashOrReached/' \
   -e 's/truncation: .*/truncation: NeverTruncate/' \
-  -e 's/illuminationRadius: .*/illuminationRadius: 6.0/' \
   {} +
